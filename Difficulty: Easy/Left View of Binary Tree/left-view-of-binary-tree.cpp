ce7d94d323831349cc8158559a6b8b54bea4d7e3
Aditi <151827388+AditiJ04@users.noreply.github.com>
2024-08-31 19:08:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -126,28 +126,29 @@ struct Node
     }
 };
  */
-void solve(Node* root,vector<int>&ans,int lev)
+
+//Function to return a list containing elements of left view of the binary tree.
+void Left(Node* root,vector<int>&ans,int lev)
 {
     if(root==NULL)
     {
         return;
     }
     
-    if(lev==ans.size())
+    if(ans.size()==lev)
     {
         ans.push_back(root->data);
     }
     
-    solve(root->left,ans,lev+1);
-    solve(root->right,ans,lev+1);
+    Left(root->left,ans,lev+1);
+    Left(root->right,ans,lev+1);
 }
-//Function to return a list containing elements of left view of the binary tree.
 vector<int> leftView(Node *root)
 {
    // Your code here
    vector<int>ans;
    
-   solve(root,ans,0);
+   Left(root,ans,0);
    
    return ans;
 }
