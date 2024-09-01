8110e693ed06c6043a4f3426ad1713e9787df2a1
Aditi <151827388+AditiJ04@users.noreply.github.com>
2024-09-01 15:25:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -105,85 +105,79 @@ struct Node
 
 class Solution {
 public:
-    void traverseLeft(Node* root,vector<int>&ans)
+    void TraverseLeft(Node* root,vector<int>&ans)
     {
         if(root==NULL || root->left==NULL && root->right==NULL)
         {
             return;
         }
+        
         ans.push_back(root->data);
+        
+        
         if(root->left)
         {
-            traverseLeft(root->left,ans);
+        TraverseLeft(root->left,ans);
         }
+        
         else
         {
-        traverseLeft(root->right,ans);
+        TraverseLeft(root->right,ans);
         }
     }
     
-    void traverseLeaf(Node* root,vector<int>&ans)
+    
+     void TraverseLeaf(Node* root,vector<int>&ans)
     {
         if(root==NULL)
         {
             return;
         }
-        
         if(root->left==NULL && root->right==NULL)
         {
-            ans.push_back(root->data);
-            return;
+        ans.push_back(root->data);
+        return;
         }
         
-            traverseLeaf(root->left,ans);
-            traverseLeaf(root->right,ans);
+        TraverseLeaf(root->left,ans);
     
+        TraverseLeaf(root->right,ans);
     }
     
-    void traverseRight(Node* root,vector<int>&ans)
+    
+     void TraverseRight(Node* root,vector<int>&ans)
     {
-        if((root==NULL) || (root->left==NULL && root->right==NULL))
+        if(root==NULL || root->left==NULL && root->right==NULL)
         {
             return;
         }
+        
         
         if(root->right)
         {
-            traverseRight(root->right,ans);
+        TraverseRight(root->right,ans);
         }
+        
         else
         {
-            traverseRight(root->left,ans);
+        TraverseRight(root->left,ans);
         }
-        ans.push_back(root->data);
         
+          ans.push_back(root->data);
     }
     vector <int> boundary(Node *root)
     {
         //Your code here
-        
         vector<int>ans;
-        
-        if(root==NULL)
-        {
-            return ans;
-        }
-        
         ans.push_back(root->data);
+        TraverseLeft(root->left,ans);
         
-        //to traverse the left sub tree
-        traverseLeft(root->left,ans);
-        
-        //to traverse the left subtree leaf node
-        traverseLeaf(root->left,ans);
-        
-        //to traverse the right subtree leaf nodes
-        traverseLeaf(root->right,ans);
-        
-        traverseRight(root->right,ans);
+        TraverseLeaf(root->left,ans);
+         TraverseLeaf(root->right,ans);
+         
+        TraverseRight(root->right,ans);
         
         return ans;
-        
     }
 };
 
