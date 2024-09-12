46b9558ffaa9dc5604c1fdec68563fa8aa41594f
Aditi <151827388+AditiJ04@users.noreply.github.com>
2024-09-12 08:56:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	int m=1000000000+7;
+	int CountSubsets(int arr[],int n,int sum,vector<vector<int>>&dp)
+	{
+	    if(sum==0 && n==0)
+	    {
+	        return 1;
+	    }
+	    if(n==0)
+	    {
+	        return 0;
+	    }
+	    
+	    if(dp[n][sum]!=-1)
+	    {
+	        return dp[n][sum];
+	    }
+	    int a,b;
+	    if(arr[n-1]<=sum)
+	    {
+	    a=CountSubsets(arr,n-1,sum-arr[n-1],dp)%m;
+	    b=CountSubsets(arr,n-1,sum,dp)%m;
+	    
+	    return dp[n][sum]=(a+b)%m;
+	    }
+	   return dp[n][sum]=CountSubsets(arr,n-1,sum,dp)%m;
+	}
+	int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+       return CountSubsets(arr,n,sum,dp)%m;
+        
+	}
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
