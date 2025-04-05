6300899ef1ce4f31f34134890cc509d7826336e4
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-04-05 23:08:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -4,60 +4,45 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
   
-    int LCS(string str1,string str2,int n,int m)
-    {
-        int dp[n+1][m+1];
-        
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(i==0 || j==0)
-                {
-                dp[i][j]=0;
-                }
-            }
-        }
-        
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(str1[i-1]==str2[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=0;
-                }
-            }
-        }
-        
-        int mx=INT_MIN;
-        
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                mx=max(mx,dp[i][j]);
-            }
-        }
-        
-        return mx;
+  int dp[1001][1001]; // adjust size according to constraints
+int res;
+
+int solve(string& s1, string& s2, int i, int j) {
+    if (i == 0 || j == 0)
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    solve(s1, s2, i - 1, j);   // explore other subproblems
+    solve(s1, s2, i, j - 1);   // explore other subproblems
+
+    if (s1[i - 1] == s2[j - 1]) {
+        dp[i][j] = 1 + solve(s1, s2, i - 1, j - 1);
+        res = max(res, dp[i][j]);
+    } else {
+        dp[i][j] = 0;
     }
-    int longestCommonSubstr(string str1, string str2) {
+
+    return dp[i][j];
+}
+    int longestCommonSubstr(string& s1, string& s2) {
         // your code here
         
-        int n=str1.length();
-        int m=str2.length();
-        
-        return LCS(str1,str2,n,m);
+    int n = s1.length();
+    int m = s2.length();
+    res = 0;
+    memset(dp, -1, sizeof(dp));
+
+    solve(s1, s2, n, m);
+    return res;
     }
 };
+
 
 //{ Driver Code Starts.
 
@@ -70,6 +55,10 @@ int main() {
         Solution ob;
 
         cout << ob.longestCommonSubstr(s1, s2) << endl;
+
+        cout << "~"
+             << "\n";
     }
 }
+
 // } Driver Code Ends
