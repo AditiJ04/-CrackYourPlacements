282d3878e1016ddfb7de35d2766c313337678fd4
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-07-31 12:37:38 +0530

    Add solution for Minimum Size Subarray Sum

--- LeetCode/Minimum-Size-Subarray-Sum/Solution.cpp
+++ LeetCode/Minimum-Size-Subarray-Sum/Solution.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int sub=INT_MAX;
         int n=nums.size();
-        int sum=0;
+
         int i=0,j=0;
+        int sum=0;
+        int mxlen=INT_MAX;
         while(i<n)
         {
-           
             sum+=nums[i];
-              //  cnt+=1;
-                if(sum==target)
-                {
-                    sub=min(sub,i-j+1);
-                }
-                else if(sum>target)
-                {
-                    while(sum>=target && j<=i)
-                    {
-                        sub=min(sub,i-j+1);
-                        sum-=nums[j];
-                        j++;
-                    }
-                }
+            if(sum==target)
+            {
+                mxlen=min(mxlen,i-j+1);
+                sum-=nums[j];
+                j++;
+            }
+
+            else
+            {
+            while(sum>=target)
+            {
+                  mxlen=min(mxlen,i-j+1);
+                sum-=nums[j];
+                j++;
+            }
+
+            }
             i++;
         }
-        
-           if(sum>=target)
-           {
-                sub=min(sub,i-j+1);
-           }
-    if(sub==INT_MAX)
-    {
-        return 0;
-    }
-    return sub;
+
+        if(mxlen==INT_MAX)
+        {
+            return 0;
+        }
+        return mxlen;
     }
 };
