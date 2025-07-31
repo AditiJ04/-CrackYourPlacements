acd5b53503ed636c2e79e260c3722c27e912a97b
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-01 00:42:22 +0530

    Add solution for Coin Change

--- LeetCode/Coin-Change/Solution.cpp
+++ LeetCode/Coin-Change/Solution.cpp
@@ -1,30 +1,61 @@
 class Solution {
 public:
-    int Coin(vector<int>&coins,int amount,int idx,vector<vector<int>>&dp)
+    int knapsack(vector<int>&coins,int amt)
     {
-        if(amount==0)
-        {
-            return 0;
-        }
-
-        if(idx<0 || amount<0)
-        {
-            return INT_MAX-1;
-        }
-        if(dp[idx][amount]!=-1)
-        {
-            return dp[idx][amount];
-        }
-        int one=Coin(coins,amount,idx-1,dp);
-        int two=1+Coin(coins,amount-coins[idx],idx,dp);
-        return dp[idx][amount]=min(one,two);
-    }
-    int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
 
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=Coin(coins,amount,n-1,dp);
+        int dp[n+1][amt+1];
 
-        return (ans<INT_MAX-1)?ans:-1;
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=amt;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=INT_MAX-1;
+                }
+
+                if(j==0)
+                {
+                    dp[i][j]=0;
+                }
+
+                if(i==1)
+                {
+                    if(j%coins[i-1]==0)
+                    {
+                        dp[i][j]=j/coins[i-1];
+                    }
+                    else
+                    {
+                        dp[i][j]=INT_MAX-1;
+                    }
+                }
+            }
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=amt;j++)
+            {
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+
+        if(dp[n][amt]==INT_MAX-1)
+        {
+            return -1;
+        }
+        return dp[n][amt];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        return knapsack(coins,amount);
     }
 };
