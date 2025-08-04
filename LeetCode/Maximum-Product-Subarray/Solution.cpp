0b738ba9c1af77827a2c6b2a23a0515a5de59de0
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-04 23:59:17 +0530

    Add solution for Maximum Product Subarray

--- LeetCode/Maximum-Product-Subarray/Solution.cpp
+++ LeetCode/Maximum-Product-Subarray/Solution.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        double maxi = INT_MIN;
-        double prod=1;
+        int n=nums.size();
+        int mx=INT_MIN;
 
-        for(int i=0;i<nums.size();i++)
-        {
-          prod*=nums[i];
-          maxi=max(prod,maxi);
-          if(prod==0)
-           prod=1;
-        }
-        prod=1;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-          prod*=nums[i];
+        int pre=1;
+        int suff=1;
 
-          maxi=max(prod,maxi);
-          if(prod==0)
-           prod=1;
+        for(int i=0;i<n;i++)
+        {
+            if(pre==0) pre=1;
+            if(suff==0) suff=1;
+
+            pre*=nums[i];
+            suff*=nums[n-i-1];
+
+            mx=max({mx,pre,suff});
         }
-        return (int)maxi;
+
+        return mx;
     }
 };
