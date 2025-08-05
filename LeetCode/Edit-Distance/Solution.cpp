38103010d26ce2ba2e688eb2192f7ac77254031e
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-05 15:43:39 +0530

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.cpp
+++ LeetCode/Edit-Distance/Solution.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    
-    int minOperations(string word1,string word2,vector<vector<int>>&dp,int idx1,int idx2)
-    {
-        int n=word1.size();
-        int m=word2.size();
-       if (idx1 == word1.size()) return word2.size() - idx2;
-if (idx2 == word2.size()) return word1.size() - idx1;
-        
-
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        int o2=0,o3=0,o4=0;
-
-        if(word1[idx1]==word2[idx2])
-        {
-            return dp[idx1][idx2]=minOperations(word1,word2,dp,idx1+1,idx2+1);
-        }
-        else
-        {
-            o2=minOperations(word1,word2,dp,idx1+1,idx2);
-            o3=minOperations(word1,word2,dp,idx1,idx2+1);
-            o4=minOperations(word1,word2,dp,idx1+1,idx2+1);
-
-            return dp[idx1][idx2]=1+min({o4,o2,o3});
-        }
-    }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return minOperations(word1,word2,dp,0,0);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i=0;i<n;i++)
+        {
+            dp[i][m]=n-i;
+        }
+
+        for(int i=0;i<m;i++)
+        {
+            dp[n][i]=m-i;
+        }
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                if(word1[i]==word2[j])
+                {
+                    dp[i][j]=dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j]=1+min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
