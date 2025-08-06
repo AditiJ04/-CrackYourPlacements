dd8f722f2aa40b9765bd2f3955f985729041a8ec
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-07 00:07:50 +0530

    Add solution for Subsets

--- LeetCode/Subsets/Solution.cpp
+++ LeetCode/Subsets/Solution.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    set<vector<int>>st;
-    void findSubset(vector<int>&nums,vector<int>&v,int idx)
-    {
-        st.insert(v);
-        if(idx==nums.size()) return;
-
-        v.push_back(nums[idx]);
-        findSubset(nums,v,idx+1);
-        v.pop_back();
-        findSubset(nums,v,idx+1);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-
-        vector<vector<int>>ans;
-        vector<int>v;
-        findSubset(nums,v,0);
-
-        ans.assign(st.begin(),st.end());
-        return ans;
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
     }
 };
