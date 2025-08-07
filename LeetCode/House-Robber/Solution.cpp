be3a67a93fdda764719596ba74aab88e40fb4f12
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-07 23:55:42 +0530

    Add solution for House Robber

--- LeetCode/House-Robber/Solution.cpp
+++ LeetCode/House-Robber/Solution.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
+
+    int t[101];
+    int Rob(int idx,vector<int>&nums)
+    {
+        if(idx>=nums.size())
+        {
+            return 0;
+        }
+
+        if(t[idx]!=-1)
+        {
+            return t[idx];
+        }
+       int steal=nums[idx]+Rob(idx+2,nums);
+       int skip=Rob(idx+1,nums);
+
+       return t[idx]=max(steal,skip);
+    }
     int rob(vector<int>& nums) {
-       int n=nums.size();
-
-       if(n==1)
-       {
-        return nums[0];
-       }
-       vector<int>t(n+1,0);
-
-       //t[i]=max stolen money till i house
-
-       //no house : i=0;
-       t[0]=0;
-
-       //1 house:i=1
-       t[1]=nums[0];
-
-       for(int i=2;i<=n;i++)
-       {
-        int steal=nums[i-1]+t[i-2];
-        int skip=t[i-1];
-
-        t[i]=max(steal,skip);
-
-       }
-
-       return t[n]; 
+        memset(t,-1,sizeof(t));
+        return Rob(0,nums);
     }
 };
