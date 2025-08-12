85f1ec02ee635ee8cc2cdb772e895d5fe57f17f4
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-12 07:25:53 +0530

    Add solution for Flood Fill

--- LeetCode/Flood-Fill/Solution.cpp
+++ LeetCode/Flood-Fill/Solution.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-
-    int row[4]={0,-1,0,1};
-    int col[4]={-1,0,1,0};
-
-    void fillImage(int sr,int sc,int color,vector<vector<int>>&image,int oricolor)
+    
+    void dfs(vector<vector<int>>&image,int sr,int sc,int oricolor,int color)
     {
         int n=image.size();
         int m=image[0].size();
 
         image[sr][sc]=color;
 
+        int row[4]={0,-1,0,1};
+        int col[4]={-1,0,1,0};
+
         for(int i=0;i<4;i++)
         {
             int nr=sr+row[i];
             int nc=sc+col[i];
 
-            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==oricolor)
+            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==oricolor) 
             {
-                fillImage(nr,nc,color,image,oricolor);
+                dfs(image,nr,nc,oricolor,color);
             }
         }
+
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-
-        if(image[sr][sc]==color) return image;
-        fillImage(sr,sc,color,image,image[sr][sc]);
+        
+      
+        int oricolor=image[sr][sc];
+         if (oricolor != color)
+         {
+        dfs(image, sr, sc, oricolor, color);
+         }
 
         return image;
     }
