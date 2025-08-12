2874f45498cf6b1b5585fb8750ce6c8122930111
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-12 07:26:08 +0530

    Add solution for Flood Fill

--- LeetCode/Flood-Fill/Solution.cpp
+++ LeetCode/Flood-Fill/Solution.cpp
@@ -1,37 +1,50 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>>&image,int sr,int sc,int oricolor,int color)
-    {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
         int m=image[0].size();
 
-        image[sr][sc]=color;
+        int row[]={0,-1,0,+1};
+        int col[]={-1,0,+1,0};
 
-        int row[4]={0,-1,0,1};
-        int col[4]={-1,0,1,0};
+        vector<vector<int>>ans=image;
 
-        for(int i=0;i<4;i++)
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        queue<pair<int,int>>que;
+
+        que.push({sr,sc});
+
+        vis[sr][sc]=1;
+        ans[sr][sc]=color;
+
+        while(!que.empty())
         {
-            int nr=sr+row[i];
-            int nc=sc+col[i];
+            int s=que.size();
 
-            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==oricolor) 
+            while(s--)
             {
-                dfs(image,nr,nc,oricolor,color);
+            int r=que.front().first;
+            int c=que.front().second;
+
+            que.pop();
+            for(int i=0;i<4;i++)
+            {
+                int nr=r+row[i];
+                int nc=c+col[i];
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m && ans[nr][nc]==image[sr][sc] && !vis[nr][nc])
+                {
+                    ans[nr][nc]=color;
+                    vis[nr][nc]=1;
+
+                    que.push({nr,nc});
+                }
+            }
+
             }
         }
 
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-      
-        int oricolor=image[sr][sc];
-         if (oricolor != color)
-         {
-        dfs(image, sr, sc, oricolor, color);
-         }
-
-        return image;
+        return ans;
     }
 };
