c6d73e35a9f09fe86dda3314b924c7df8b173019
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-13 03:58:48 +0530

    Add solution for Is Graph Bipartite?

--- LeetCode/Is-Graph-Bipartite?/Solution.cpp
+++ LeetCode/Is-Graph-Bipartite?/Solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    bool dfs(int node,int n,vector<int>adj[],vector<int>&color,int col)
+    bool dfs(int node,int col,vector<int>&color,vector<int>adj[])
     {
+        
         color[node]=col;
+        
 
-        for(auto it:adj[node])
+        for(auto c:adj[node])
         {
-            if(color[it]==-1)
+            if(color[c]==-1)
             {
-                if((dfs(it,n,adj,color,!col))==false)
+                if(dfs(c,!col,color,adj)==false)
                 {
                     return false;
                 }
             }
-            else if(color[it]==col)
+            else if(color[c]==col)
             {
                 return false;
             }
@@ -21,29 +23,25 @@ public:
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        
         int n=graph.size();
-
-        vector<int>color(n,-1);
-
         vector<int>adj[n];
-
         for(int i=0;i<n;i++)
         {
-            for(int num:graph[i])
+            for(auto it:graph[i])
             {
-                adj[i].push_back(num);
+                adj[i].push_back(it);
             }
         }
 
+        vector<int>color(n,-1);
         for(int i=0;i<n;i++)
         {
             if(color[i]==-1)
             {
-            if(dfs(i,n,adj,color,0)==false)
-            {
-                return false;
-            }
+        if(dfs(i,0,color,adj)==false)
+        {
+            return false;
+        }
             }
         }
         return true;
