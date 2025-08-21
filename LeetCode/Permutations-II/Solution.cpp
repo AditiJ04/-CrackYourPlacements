f2de7d4c1e03dca780a9aee468dfaeada2288f2a
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-21 13:28:37 +0530

    Add solution for Permutations II

--- LeetCode/Permutations-II/Solution.cpp
+++ LeetCode/Permutations-II/Solution.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    void Permute(vector<int>nums,vector<vector<int>>&ans,int idx)
+    void permut(vector<int>nums,vector<vector<int>>&ans,int start)
     {
-        if(idx==nums.size())
+        if(start==nums.size())
         {
             ans.push_back(nums);
             return;
         }
-
         else
         {
-        
-            for(int j=idx;j<nums.size();j++)
+            for(int j=start;j<nums.size();j++)
             {
-                if(j==idx || nums[j]!=nums[idx])
+                if(j==start || nums[j]!=nums[start])
                 {
-                swap(nums[j],nums[idx]);
-                Permute(nums,ans,idx+1);
+                    swap(nums[j],nums[start]);
+                    permut(nums,ans,start+1);
                 }
-
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n=nums.size();
+        sort(nums.begin(),nums.end());
 
         vector<vector<int>>ans;
 
-        sort(nums.begin(),nums.end());
-        Permute(nums,ans,0);
+        permut(nums,ans,0);
 
         return ans;
     }
