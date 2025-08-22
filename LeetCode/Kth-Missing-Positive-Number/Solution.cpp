c424d1a28c780e8a69b76be2a728e070f9e9fed8
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-22 07:39:52 +0530

    Add solution for Kth Missing Positive Number

--- LeetCode/Kth-Missing-Positive-Number/Solution.cpp
+++ LeetCode/Kth-Missing-Positive-Number/Solution.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        unordered_map<int,int>mp;
-
         int n=arr.size();
-        int mx=*max_element(arr.begin(),arr.end());
 
         for(int i=0;i<n;i++)
         {
-            mp[arr[i]]++;
-        }
-
-        for(int i=1;i<=mx;i++)
-        {
-            if(mp.find(i)==mp.end())
+            if(arr[i]<=k)
             {
-                k--;
-                if(k==0) return i;
+                k++;
             }
+            else break;
         }
-        return mx+k;
+        return k;
     }
 };
