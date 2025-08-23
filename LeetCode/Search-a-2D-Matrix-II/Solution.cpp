2b2e156ec49ccde06624e57b2605ea2341adaa84
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-23 05:48:50 +0530

    Add solution for Search a 2D Matrix II

--- LeetCode/Search-a-2D-Matrix-II/Solution.cpp
+++ LeetCode/Search-a-2D-Matrix-II/Solution.cpp
@@ -1,47 +1,25 @@
 class Solution {
 public:
-    int findUB(vector<int>&arr,int m,int num)
-    {
-        int l=0;
-        int h=m-1;
-        int ans=m;
-
-        while(l<=h)
-        {
-            int mid=l+(h-l)/2;
-
-            if(arr[mid]>num)
-            {
-                ans=mid;
-                h=mid-1;
-            }
-            else
-            {
-                l=mid+1;
-            }
-        }
-
-        return ans;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
         int m=matrix[0].size();
 
-        if(n==1 && m==1)
+        int l=0;
+        int h=m-1;
+
+        while(l<n && h>=0)
         {
-            if(matrix[0][0]==target)
+            if(matrix[l][h]==target)
             {
                 return true;
             }
-        }
-
-        for(int i=0;i<n;i++)
-        {
-            int num=findUB(matrix[i],m,target-1);
-
-            if(num<m && matrix[i][num]==target)
+            else if(matrix[l][h]>target)
             {
-                return true;
+                h--;
+            }
+            else
+            {
+                l++;
             }
         }
 
