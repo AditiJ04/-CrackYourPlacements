7411e8aa5220d6e00ce14ea2ec9d7535ad195e58
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-25 05:18:12 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.cpp
+++ LeetCode/Valid-Parentheses/Solution.cpp
@@ -1,79 +1,21 @@
 class Solution {
 public:
-    int generateP(char ch)
-    {
-        if(ch=='(')
-        {
-            return 1;
-        }
-
-        if(ch=='[')
-        {
-            return 2;
-        }
-
-        if(ch=='{')
-        {
-            return 3;
-        }
-
-
-        if(ch==')')
-        {
-            return -1;
-        }
-
-        if(ch==']')
-        {
-            return -2;
-        }
-
-        return -3;
-    }
     bool isValid(string s) {
-        stack<int>st;
-
-        int n=s.size();
-
-        for(int i=0;i<n;i++)
-        {
-
-            if(st.empty())
-            {
-                int val=generateP(s[i]);
-                if(val<0)
-                {
-                    return false;
-                }
-                st.push(val);
-            }
-
-            else 
-            {
-                int val=generateP(s[i]);
-
-                if(st.top()>0 && val>0)
-                {
-                    st.push(val);
-                }
-                else 
-                {
-                    if(st.top()==abs(val))
-                    {
-                        st.pop();
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
+        int top = -1;
+        for(int i =0;i<s.length();++i){
+            if(top<0 || !isMatch(s[top], s[i])){
+                ++top;
+                s[top] = s[i];
+            }else{
+                --top;
             }
         }
-
-        if(!st.empty())
-        {
-            return false;
-        }
-        return true;
+        return top == -1;
+    }
+    bool isMatch(char c1, char c2){
+        if(c1 == '(' && c2 == ')') return true;
+        if(c1 == '[' && c2 == ']') return true;
+        if(c1 == '{' && c2 == '}') return true;
+        return false;
     }
 };
