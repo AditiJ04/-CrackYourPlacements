091cbcb775ac9c1be334e0d6daffc58bc5c5b2d8
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-28 04:55:02 +0530

    Add solution for Max Consecutive Ones III

--- LeetCode/Max-Consecutive-Ones-III/Solution.cpp
+++ LeetCode/Max-Consecutive-Ones-III/Solution.cpp
@@ -2,30 +2,24 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n=nums.size();
-        int zc=0;
+
+        int l=0,r=0,cnt=0;
         int mx=0;
-        int l=0,r=0;
 
         while(r<n)
         {
             if(nums[r]==0)
             {
-                zc++;
+                cnt++;
             }
-
-            while(zc>k)
+            while(cnt>k)
             {
-                if(nums[l]==0)
-                {
-                    zc--;
-                }
-                    l++;
-                
+                if(nums[l]==0) cnt--;
+                l++;
             }
             mx=max(mx,r-l+1);
             r++;
         }
-
         return mx;
     }
 };
