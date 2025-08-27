8f3040b4da1084d65842bcb5426fa7e9880ff885
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-27 10:21:28 +0530

    Add solution for Best Time to Buy and Sell Stock III

---
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock-III/Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    int mxpro=0;
+    int profitEarned(vector<int>&prices, vector<vector<vector<int>>>&dp,int idx,int canbuy,int trans)
+    {
+        int n=prices.size();
+
+        if(idx==n || trans<=0) return 0;
+        if(dp[idx][canbuy][trans]!=-1) return dp[idx][canbuy][trans];
+        int buy=INT_MAX,nbuy=INT_MAX;
+
+        if(canbuy)
+        {
+            buy=profitEarned(prices,dp,idx+1,!canbuy,trans)-prices[idx];
+        }
+        else
+        {
+            buy=prices[idx]+profitEarned(prices,dp,idx+1,!canbuy,trans-1);
+        }
+        nbuy=profitEarned(prices,dp,idx+1,canbuy,trans);
+        return dp[idx][canbuy][trans]=mxpro+max(buy,nbuy);
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n + 1,vector<vector<int>>(2, vector<int>(3, -1) ));
+        return profitEarned(prices,dp,0,1,2);
+    }
+};
