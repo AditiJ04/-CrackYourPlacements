a4de3d66c5e61456508f39d6147d539cfea82961
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-27 12:07:34 +0530

    Add solution for Best Time to Buy and Sell Stock with Cooldown

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/Solution.cpp
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/Solution.cpp
@@ -1,28 +1,13 @@
-class Solution {
-public:
-    int mxpro=0;
-
-    int totalProfit(vector<int>&prices,vector<vector<int>>&dp,int idx,int canBuy)
-    {
-        if(idx>=prices.size()) return 0;
-        if(dp[idx][canBuy]!=-1) return dp[idx][canBuy];
-        int buy=INT_MIN,nbuy=INT_MAX;
-
-        if(canBuy)
-        {
-            buy=totalProfit(prices,dp,idx+1,!canBuy)-prices[idx];
-        }
-        else
-        {
-            buy=totalProfit(prices,dp,idx+2,!canBuy)+prices[idx];
-        }
-        nbuy=totalProfit(prices,dp,idx+1,canBuy);
-        return dp[idx][canBuy]=mxpro+max(buy,nbuy);
+int solve(int buyedStock,int i,vector<int>&prices){
+    if(i>=prices.size()){
+        return 0;
     }
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        return totalProfit(prices,dp,0,1);
+    if(buyedStock==-1){
+        buyedStock=prices[i];
     }
-};
+    if(dp[i]!=-1) return dp[i];
+    int notbought=solve(-1,i+1,prices);
+    int sold=prices[i]-buyedStock+solve(-1,i+2,prices);
+    int notSold=solve(buyedStock,i+1,prices);
+    return dp[i]=max({sold,notSold,notbought});
+}
