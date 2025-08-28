ab3aa87c4adc9609cb699bb5de8fcf979f176af9
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-28 07:42:18 +0530

    Add solution for Ugly Number

--- LeetCode/Ugly-Number/Solution.cpp
+++ LeetCode/Ugly-Number/Solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        
+
         if(n==0) return false;
         if(n==1) return true;
-        while(n!=1)
+        unordered_map<int,int>mp;
+        mp[2]++;
+        mp[3]++;
+        mp[5]++;
+
+        for(auto it:mp)
         {
-        if((n%2)==0)
-        {
-            n/=2;
-        }
-        else if(n%3==0)
-        {
-            n/=3;
-        }
-        else if(n%5==0)
-        {
-            n/=5;
-        }
-        else
-        {
-            return false;
-        }
+            while(n%it.first==0)
+            {
+                n/=it.first;
+            }
         }
 
-        return true;
+        return n==1;
     }
 };
