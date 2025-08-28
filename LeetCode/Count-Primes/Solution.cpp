dd8a71c6dcd585431f8fb20c1644fae19c8b94c6
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-08-28 07:53:09 +0530

    Add solution for Count Primes

--- LeetCode/Count-Primes/Solution.cpp
+++ LeetCode/Count-Primes/Solution.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int Checkprime(int n)
-    {
-        vector<int>Prime(n+1,1);
-        int cnt=0;
-
-        Prime[0]=Prime[1]=0;
-
-        for(int i=2;i<=sqrt(n);i++)
-        {
-            if(Prime[i]==1)
-            {
-                for(int j=i*i;j<=n;j+=i)
-                {
-                    Prime[j]=0;
-                }
-            }
-        }
-
-        for(int i=2;i<n;i++)
-        {
-            if(Prime[i]==1) cnt++;
-        }
-        return cnt;
-    }
     int countPrimes(int n) {
 
         if(n==0 || n==1 || n==2) return 0;
-    
-        return Checkprime(n);
+        vector<int>P(n+1,1);
+
+        P[0]=P[1]=0;
+        for(int i=2;i<=sqrt(n);i++)
+        {
+            if(P[i]==1)
+            {
+              for(int j=i*i;j<=n;j+=i)
+              {
+                P[j]=0;
+              }
+            }
+        }
+
+        int cnt=0;
+
+        for(int i=2;i<n;i++)
+        {
+            if(P[i]==1) cnt++;
+        }
+
+        return cnt;
     }
 };
