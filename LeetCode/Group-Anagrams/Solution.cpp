f610fb030127bec108d5bb1517e30f6a4b8b6077
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-05 00:07:42 +0530

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.cpp
+++ LeetCode/Group-Anagrams/Solution.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
+    string generate(string str)
+    {
+        int n=str.size();
+        int arr[26]={0};
+
+        for(char &ch:str)
+        {
+            arr[ch-'a']++;
+        }
+
+        string new_word="";
+
+        for(int i=0;i<26;i++)
+        {
+            int freq=arr[i];
+
+            if(freq>0)
+            {
+                new_word+=string(freq,i+'a');
+            }
+        }
+
+        return new_word;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n=strs.size();
 
         vector<vector<string>>ans;
         unordered_map<string,vector<string>>mp;
 
-        for(auto str:strs)
+        for(int i=0;i<n;i++)
         {
-            string s=str;
-            sort(str.begin(),str.end());
-            mp[str].push_back(s);
+            string str=strs[i];
+            string new_word=generate(str);
+            mp[new_word].push_back(str);
         }
 
         for(auto it:mp)
@@ -19,5 +43,6 @@ public:
         }
 
         return ans;
+        
     }
 };
