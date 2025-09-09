d5592dfa51bb492a89590e86d2566c93e9ffb20f
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-09 17:31:26 +0530

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.cpp
+++ LeetCode/Majority-Element/Solution.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+
         int n=nums.size();
-        int ans=0;
+        int ans=nums[n-1];
+        sort(nums.begin(),nums.end());
 
-        unordered_map<int,int>mp;
-
-        for(int i=0;i<n;i++)
+        int prev=0;
+        for(int i=1;i<n;i++)
         {
-            mp[nums[i]]++;
-        }
-
-        for(auto it:mp)
-        {
-            if(it.second>ceil(n/2))
+            if(nums[i]!=nums[i-1])
             {
-                ans=it.first;
-                break;
+                int cnt=i-prev;
+
+                if(cnt>ceil(n/2)) 
+                {
+                    return nums[i-1];
+                }
+                prev=i;
             }
         }
+        if(n-prev>ceil(n/2)) return nums[n-1];
 
-        return ans;
+        return -1;
     }
 };
+
+// 2 2 2 3 3 3 3 
+// 0 1 2 3 4 5 6
