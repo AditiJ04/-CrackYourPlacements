0d738e0b3fb1dfa51ac15727d58c7087e603b197
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-10 23:27:38 +0530

    Add solution for Number of Ways to Reach a Position After Exactly k Steps

--- LeetCode/Number-of-Ways-to-Reach-a-Position-After-Exactly-k-Steps/Solution.cpp
+++ LeetCode/Number-of-Ways-to-Reach-a-Position-After-Exactly-k-Steps/Solution.cpp
@@ -1,25 +1,26 @@
-class Solution 
-{
-    map<pair<int, int>, int> dp;
-    const long long int MOD = 1e9+7;
-
-    int solve(int curr, int end, int k)
-    {
-        if(k == 0)
-            return (curr == end)? 1:0;
-        
-        
-        if(dp.find({curr, k}) != dp.end())
-            return dp[{curr, k}];
-        
-        int left = solve(curr - 1, end, k - 1);
-        int right = solve(curr + 1, end, k - 1);
-
-        return dp[{curr, k}] = (left + right) % MOD;
-    }
-
+class Solution {
 public:
+    int mod=1e9+7;
+
+    int f(int s,int i,int j,int k,int e,vector<vector<int>>&dp)
+    {
+        if(i==k && j==e) return 1;
+        if(i==k && j!=e) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int l=0,r=0;
+        l=f(s,i+1,j-1,k,e,dp);
+        r=f(s,i+1,j+1,k,e,dp);
+
+        return dp[i][j]=(l+r)%mod;
+    }
     int numberOfWays(int startPos, int endPos, int k) {
-        return solve(startPos, endPos, k) % MOD;    
+        vector<vector<int>>dp(k+1,vector<int>(10000,-1));
+
+        startPos=startPos+1000;
+        endPos=endPos+1000;
+
+        return f(startPos,0,startPos,k,endPos,dp)%mod;
     }
 };
