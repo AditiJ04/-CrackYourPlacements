382e1aad21800bde720783fe9f5e034adcc4f7a3
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-15 00:37:26 +0530

    Add solution for Rearrange Array Elements by Sign

--- LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
+++ LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
@@ -1,32 +1,3 @@
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
-
-        vector<int>newA;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>=0)
-            {
-                pos.push_back(nums[i]);
-            }
-            else
-            {
-                neg.push_back(nums[i]);
-            }
-        }
-       
-       int i=0,j=0;
-
-       while(i<pos.size() && j<neg.size())
-       {
-           newA.push_back(pos[i]);
-           newA.push_back(neg[i]);
-           i++;
-           j++;
-       }
-        return newA;
-    }
-};
+positive = (num for num in nums if num >= 0)
+negative = (num for num in nums if num < 0)
+return [x for x in itertools.chain(*zip(positive, negative))]
