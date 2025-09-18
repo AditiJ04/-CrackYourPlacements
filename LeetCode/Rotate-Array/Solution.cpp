63b6937bd593dd0da2af6746fd635abf73c3e765
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-18 16:47:14 +0530

    Add solution for Rotate Array

--- LeetCode/Rotate-Array/Solution.cpp
+++ LeetCode/Rotate-Array/Solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-    
+
         int n=nums.size();
-        vector<int>v(n);
+        vector<int>v(n,0);
 
         for(int i=0;i<n;i++)
         {
-            int idx=(i+k)%n;
-            v[idx]=nums[i];
+            v[(i+k)%n]=nums[i];
         }
 
         nums=v;
