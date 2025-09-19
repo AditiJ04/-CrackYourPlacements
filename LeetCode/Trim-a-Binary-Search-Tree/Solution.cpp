870dafbba6ea70ad98f9dea80ccf2e960bffaac8
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-19 19:06:48 +0530

    Add solution for Trim a Binary Search Tree

--- LeetCode/Trim-a-Binary-Search-Tree/Solution.cpp
+++ LeetCode/Trim-a-Binary-Search-Tree/Solution.cpp
@@ -11,26 +11,20 @@
  */
 class Solution {
 public:
-    TreeNode* Tree(TreeNode* root,int low,int high)
-    {
-        if(root==NULL) return NULL;
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root==NULL) return root;
+
+        if(root->val>=low && root->val<=high)
+        {
+            root->left=trimBST(root->left,low,high);
+            root->right=trimBST(root->right,low,high);
+            return root;
+        }
 
         if(root->val<low)
         {
-            return Tree(root->right,low,high);
+            return trimBST(root->right,low,high);
         }
-
-        if(root->val>high)
-        {
-            return Tree(root->left,low,high);
-        }
-
-        root->left=Tree(root->left,low,high);
-        root->right=Tree(root->right,low,high);
-
-        return root;
-    }
-    TreeNode* trimBST(TreeNode* root, int low, int high) {
-        return Tree(root,low,high);   
+        return trimBST(root->left,low,high);
     }
 };
