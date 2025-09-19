c11bc190d67e7b83d1b555dd9d619b8751f8ac38
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-19 19:05:07 +0530

    Add solution for Trim a Binary Search Tree

--- LeetCode/Trim-a-Binary-Search-Tree/Solution.cpp
+++ LeetCode/Trim-a-Binary-Search-Tree/Solution.cpp
@@ -1,2 +1,36 @@
-A = [1, 2, 3, 4, 5, 6, 7, 8, 9], L = 3, R = 7
-Result =  [3, 4, 5, 6, 7] 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* Tree(TreeNode* root,int low,int high)
+    {
+        if(root==NULL) return NULL;
+
+        if(root->val<low)
+        {
+            return Tree(root->right,low,high);
+        }
+
+        if(root->val>high)
+        {
+            return Tree(root->left,low,high);
+        }
+
+        root->left=Tree(root->left,low,high);
+        root->right=Tree(root->right,low,high);
+
+        return root;
+    }
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        return Tree(root,low,high);   
+    }
+};
