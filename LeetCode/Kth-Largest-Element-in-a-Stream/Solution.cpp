b3b47b4a0a63c9577f93ac57d26de1152324bbb8
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-20 04:02:16 +0530

    Add solution for Kth Largest Element in a Stream

--- LeetCode/Kth-Largest-Element-in-a-Stream/Solution.cpp
+++ LeetCode/Kth-Largest-Element-in-a-Stream/Solution.cpp
@@ -1,33 +1,4 @@
-class KthLargest {
-public:
-
-    priority_queue<int,vector<int>,greater<int>>pq;
-    int k;
-    KthLargest(int k, vector<int>& nums) {
-        this->k=k;
-
-        for(auto num:nums)
-        {
-            pq.push(num);
-            if(pq.size()>k)
-            {
-                pq.pop();
-            }
-        }
-    }
-    
-    int add(int val) {
-        pq.push(val);
-        if(pq.size()>k)
-        {
-            pq.pop();
-        }
-        return pq.top();
-    }
-};
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
+["KthLargest","add","add","add","add","add"]
+[[3,[4,2]],[1],[1],[-1],[3],[4]]
+["KthLargest","add","add","add","add","add"]
+[[1,[]],[2],[1],[-1],[3],[4]]
