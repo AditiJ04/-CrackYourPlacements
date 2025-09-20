02719146c7d66fd743de9f59473c97d5c80434c0
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-20 21:40:58 +0530

    Add solution for Construct Binary Tree from Inorder and Postorder Traversal

--- LeetCode/Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution.cpp
+++ LeetCode/Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution.cpp
@@ -11,41 +11,36 @@
  */
 class Solution {
 public:
-    unordered_map<int, int> mp;
 
-TreeNode* constructTree(vector<int>& postorder, int start_idx, int end_idx, int &post) {
-    if (start_idx > end_idx) {
-        return NULL;
-    }
+    TreeNode* solve(vector<int>&inorder,vector<int>&postorder,int s,int e,int &idx)
+    {
+        if(s>e) return NULL;
 
-    // Get the current value from the postorder array and decrement the post index
-    int val = postorder[post--];
-    int idx = mp[val];
+        int currnum=postorder[idx];
 
-    // Create the root node for the current subtree
-    TreeNode* root = new TreeNode(val);
+        int i=s;
 
-    // Recursively build the right subtree first, since we're traversing postorder
-    root->right = constructTree(postorder, idx + 1, end_idx, post);
+        for(i;i<=e;i++)
+        {
+            if(inorder[i]==currnum)
+            {
+                break;
+            }
+        }
+
+        TreeNode* root=new TreeNode(currnum);
+
+        idx--;
+        root->right=solve(inorder,postorder,i+1,e,idx);
+        root->left=solve(inorder,postorder,s,i-1,idx);
     
-    // Recursively build the left subtree
-    root->left = constructTree(postorder, start_idx, idx - 1, post);
-
-    return root;
-}
-
-TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-    int n = inorder.size();
-
-    // Map each value in the inorder traversal to its index
-    for (int i = 0; i < n; i++) {
-        mp[inorder[i]] = i;
+        return root;
     }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n=inorder.size();
 
-    // Start with the last index of postorder (the root of the tree)
-    int post = n - 1;
+        int idx=n-1;
 
-    // Construct the binary tree
-    return constructTree(postorder, 0, n - 1, post);
-}
+        return solve(inorder,postorder,0,n-1,idx);
+    }
 };
