1250583864a86048fed38f2441f533d7d16b21fa
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-20 21:35:55 +0530

    Add solution for Construct Binary Tree from Inorder and Postorder Traversal

--- LeetCode/Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution.cpp
+++ LeetCode/Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/Solution.cpp
@@ -11,32 +11,41 @@
  */
 class Solution {
 public:
-    unordered_map<int,int>mp;
-    TreeNode* constructTree(vector<int>&postorder,int start_idx,int end_idx,int &post)
-    {
-        if(start_idx>end_idx)
-        {
-            return NULL;
-        }
+    unordered_map<int, int> mp;
 
-        int val=postorder[post--];
-        int idx=mp[val];
-        
-        TreeNode* root=new TreeNode(val);
-;
-        root->right=constructTree(postorder,idx+1,end_idx,post);
-          root->left=constructTree(postorder,start_idx,idx-1,post);
-
-        return root;
+TreeNode* constructTree(vector<int>& postorder, int start_idx, int end_idx, int &post) {
+    if (start_idx > end_idx) {
+        return NULL;
     }
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n=inorder.size();
 
-        for(int i=0;i<n;i++)
-        {
-            mp[inorder[i]]=i;
-        }
-        int post=n-1;
-        return constructTree(postorder,0,n-1,post);
+    // Get the current value from the postorder array and decrement the post index
+    int val = postorder[post--];
+    int idx = mp[val];
+
+    // Create the root node for the current subtree
+    TreeNode* root = new TreeNode(val);
+
+    // Recursively build the right subtree first, since we're traversing postorder
+    root->right = constructTree(postorder, idx + 1, end_idx, post);
+    
+    // Recursively build the left subtree
+    root->left = constructTree(postorder, start_idx, idx - 1, post);
+
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    int n = inorder.size();
+
+    // Map each value in the inorder traversal to its index
+    for (int i = 0; i < n; i++) {
+        mp[inorder[i]] = i;
     }
+
+    // Start with the last index of postorder (the root of the tree)
+    int post = n - 1;
+
+    // Construct the binary tree
+    return constructTree(postorder, 0, n - 1, post);
+}
 };
