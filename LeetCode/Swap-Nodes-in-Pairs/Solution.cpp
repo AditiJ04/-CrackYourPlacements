6155fdae3a6f9b893beacf417972b051618c4ade
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-23 00:15:06 +0530

    Add solution for Swap Nodes in Pairs

--- LeetCode/Swap-Nodes-in-Pairs/Solution.cpp
+++ LeetCode/Swap-Nodes-in-Pairs/Solution.cpp
@@ -11,9 +11,13 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
 
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
         ListNode* tmp=head->next;
+
         head->next=swapPairs(tmp->next);
         tmp->next=head;
 
