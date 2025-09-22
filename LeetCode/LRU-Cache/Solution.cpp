4dc60a5dc48c507495efdbdabd8d12b58a386809
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-22 17:54:04 +0530

    Add solution for LRU Cache

--- LeetCode/LRU-Cache/Solution.cpp
+++ LeetCode/LRU-Cache/Solution.cpp
@@ -1,53 +1,61 @@
-class LinkedNode:
-    def __init__(self, key = -1, val = -1):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
-
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = dict()
-        self.head = LinkedNode()
-        self.tail = LinkedNode()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        
-        node = self.__evict(key)
-        self.__addToEnd(node)
-        return node.val
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.__delete(key)
-
-        node = LinkedNode(key, value)
-        self.cache[key] = node
-        self.__addToEnd(node)
-        
-        if len(self.cache) > self.capacity:
-            self.__delete(self.head.next.key)
+class LRUCache {
+public:
     
-    def __evict(self, key) -> LinkedNode: 
-        node = self.cache[key]
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        node.next = None
-        node.prev = None
-        return node
+    list<int>dll;
 
-    def __delete(self, key) -> None:
-        deleteNode = self.__evict(key)
-        del deleteNode
-        del self.cache[key]
+    map<int,pair<list<int>::iterator,int>>mp;
+    int n;
 
-    def __addToEnd(self, node) -> None:
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
+    void makeRecently(int key)
+    {
+        dll.erase(mp[key].first);
+        dll.push_front(key);
+
+        mp[key].first=dll.begin();
+    }
+    LRUCache(int capacity) {
+        n=capacity;
+    }
+    
+    int get(int key) {
+        if(mp.find(key)==mp.end())
+        {
+            return -1;
+        }
+
+        makeRecently(key);
+
+        return mp[key].second;
+    }
+    //most recently element in front..
+    //in brute force--mru was in front
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end())
+        {
+            mp[key].second=value;
+            makeRecently(key);
+        }
+        else
+        {
+          dll.push_front(key);
+          mp[key]={dll.begin(),value};   
+          n--;
+        }
+
+        if(n<0)
+        {
+            int k=dll.back();
+            dll.pop_back();
+            mp.erase(k);
+
+            n++;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
