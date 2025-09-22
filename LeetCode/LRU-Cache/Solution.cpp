21f2c06b90fd3870c55d1fa66c1ee08f920abbc8
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-22 17:59:36 +0530

    Add solution for LRU Cache

--- LeetCode/LRU-Cache/Solution.cpp
+++ LeetCode/LRU-Cache/Solution.cpp
@@ -1,61 +1,76 @@
 class LRUCache {
 public:
-    
-    list<int>dll;
+    class Node{
+        public: 
+            int key;
+            int val;
+            Node* prev;
+            Node* next;
 
-    map<int,pair<list<int>::iterator,int>>mp;
-    int n;
+            Node(int key, int val){
+                this->key = key;
+                this->val = val;
+            }
+    };
 
-    void makeRecently(int key)
-    {
-        dll.erase(mp[key].first);
-        dll.push_front(key);
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
 
-        mp[key].first=dll.begin();
-    }
+    int cap;
+    unordered_map<int, Node*> m;
+
     LRUCache(int capacity) {
-        n=capacity;
+        cap = capacity;
+        head -> next = tail;
+        tail -> prev = head;
+    }
+
+    void addNode(Node* newnode){
+        Node* temp = head -> next;
+
+        newnode -> next = temp;
+        newnode -> prev = head;
+
+        head -> next = newnode;
+        temp -> prev = newnode;
+    }
+
+    void deleteNode(Node* delnode){
+        Node* prevv = delnode -> prev;
+        Node* nextt = delnode -> next;
+
+        prevv -> next = nextt;
+        nextt -> prev = prevv;
     }
     
     int get(int key) {
-        if(mp.find(key)==mp.end())
-        {
-            return -1;
+        if(m.find(key) != m.end()){
+            Node* resNode = m[key];
+            int ans = resNode -> val;
+
+            m.erase(key);
+            deleteNode(resNode);
+            addNode(resNode);
+
+            m[key] = head -> next;
+            return ans;
         }
-
-        makeRecently(key);
-
-        return mp[key].second;
+        return -1;
     }
-    //most recently element in front..
-    //in brute force--mru was in front
+    
     void put(int key, int value) {
-        if(mp.find(key)!=mp.end())
-        {
-            mp[key].second=value;
-            makeRecently(key);
-        }
-        else
-        {
-          dll.push_front(key);
-          mp[key]={dll.begin(),value};   
-          n--;
+        if(m.find(key) != m.end()){
+            Node* curr = m[key];
+            m.erase(key);
+            deleteNode(curr);
         }
 
-        if(n<0)
-        {
-            int k=dll.back();
-            dll.pop_back();
-            mp.erase(k);
-
-            n++;
+        if(m.size() == cap){
+            m.erase(tail -> prev -> key);
+            deleteNode(tail -> prev);
         }
+
+        addNode(new Node(key, value));
+        m[key] = head -> next;
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
