f094edf95cdf11c4e5133ee68573b7f1cfc4fd17
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-24 20:35:05 +0530

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.cpp
+++ LeetCode/Edit-Distance/Solution.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
+    int fun(string s1,string s2,int i,int j,vector<vector<int>>&dp)
+    {
+        if(i<0)
+        {
+            return j+1;
+        }
+
+        if(j<0)
+        {
+            return i+1;
+        }
+
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j])
+        {
+            return dp[i][j]=0+fun(s1,s2,i-1,j-1,dp);
+        }
+
+        return dp[i][j]=1+min(fun(s1,s2,i,j-1,dp),min(fun(s1,s2,i-1,j,dp),fun(s1,s2,i-1,j-1,dp)));
+    }
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
+        int n=word1.length();
+        int m=word2.length();
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-
-        for(int i=0;i<n;i++)
-        {
-            dp[i][m]=n-i;
-        }
-
-        for(int i=0;i<m;i++)
-        {
-            dp[n][i]=m-i;
-        }
-
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=m-1;j>=0;j--)
-            {
-                if(word1[i]==word2[j])
-                {
-                    dp[i][j]=dp[i+1][j+1];
-                }
-                else
-                {
-                    dp[i][j]=1+min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
-                }
-            }
-        }
-
-        return dp[0][0];
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return fun(word1,word2,n-1,m-1,dp);
     }
 };
