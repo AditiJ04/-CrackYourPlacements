2edb852f737454bbb6e5e9bab83c74bbdcbb376c
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-24 20:35:26 +0530

    Add solution for Edit Distance

--- LeetCode/Edit-Distance/Solution.cpp
+++ LeetCode/Edit-Distance/Solution.cpp
@@ -1,60 +1,36 @@
 class Solution {
 public:
-    int fun(string s1,string s2,int n,int m,vector<vector<int>>&dp)
-    {
-       /* if(i<0)
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i=0;i<n;i++)
         {
-            return j+1;
+            dp[i][m]=n-i;
         }
 
-        if(j<0)
+        for(int i=0;i<m;i++)
         {
-            return i+1;
+            dp[n][i]=m-i;
         }
 
-        if(dp[i][j]!=-1)
+        for(int i=n-1;i>=0;i--)
         {
-            return dp[i][j];
-        }
-        if(s1[i]==s2[j])
-        {
-            return dp[i][j]=0+fun(s1,s2,i-1,j-1,dp);
-        }
-
-        return dp[i][j]=1+min(fun(s1,s2,i,j-1,dp),min(fun(s1,s2,i-1,j,dp),fun(s1,s2,i-1,j-1,dp)))*/
-
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0]=i;
-        }
-
-        for(int j=0;j<=m;j++)
-        {
-            dp[0][j]=j;
-        }
-
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
+            for(int j=m-1;j>=0;j--)
             {
-                if(s1[i-1]==s2[j-1])
+                if(word1[i]==word2[j])
                 {
-                    dp[i][j]=dp[i-1][j-1];
+                    dp[i][j]=dp[i+1][j+1];
                 }
                 else
                 {
-                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                    dp[i][j]=1+min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
                 }
             }
         }
 
-        return dp[n][m];
-    }
-    int minDistance(string word1, string word2) {
-        int n=word1.length();
-        int m=word2.length();
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return fun(word1,word2,n,m,dp);
+        return dp[0][0];
     }
 };
