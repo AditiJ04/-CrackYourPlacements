d3b95c8c6bd36a96cd27c1f3ae41f3f947879722
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-24 23:41:46 +0530

    Add solution for Nth Highest Salary

--- LeetCode/Nth-Highest-Salary/Solution.sql
+++ LeetCode/Nth-Highest-Salary/Solution.sql
@@ -1,12 +1,10 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-
   set N=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      select distinct salary 
-      from Employee
-      order by salary desc 
-      LIMIT 1 OFFSET N
+      select distinct salary from Employee
+      order by salary desc limit 1 offset N
+
   );
 END
