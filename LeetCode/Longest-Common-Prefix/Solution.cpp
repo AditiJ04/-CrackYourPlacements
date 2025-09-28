50a4a487dff71d8a93c5f069ad6ee706d9fc1db9
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-29 02:24:11 +0530

    Add solution for Longest Common Prefix

---
+++ LeetCode/Longest-Common-Prefix/Solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+
+        sort(strs.begin(),strs.end());
+
+        string str1=strs[0];
+        string str2=strs[n-1];
+
+        string ans="";
+
+        for(int i=0;i<str1.size();i++)
+        {
+            if(str1[i]==str2[i])
+            {
+                ans+=str1[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        return ans;
+    }
+};
