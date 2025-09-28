21a278640ecdea37fe30cbe5b08e68a57f635523
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-28 05:47:28 +0530

    Add solution for Single Element in a Sorted Array

--- LeetCode/Single-Element-in-a-Sorted-Array/Solution.cpp
+++ LeetCode/Single-Element-in-a-Sorted-Array/Solution.cpp
@@ -2,30 +2,29 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& arr) {
         int n = arr.size();
-        if (n == 1)
-            return arr[0];
+
+        if(n==1) return arr[0];
         if (arr[0] != arr[1])
             return arr[0];
         if (arr[n - 1] != arr[n - 2])
             return arr[n - 1];
 
-        int low = 1, high = n - 2;
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int l = 1;
+        int h = n - 2;
 
-            if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+        while (l <= h) {
+            int mid = (l + h) / 2;
+
+            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1])
                 return arr[mid];
-            }
             if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) ||
                 (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
-                low = mid + 1;
-            }
-
-            else {
-
-                high = mid - 1;
+                l = mid + 1;
+            } else {
+                h = mid - 1;
             }
         }
+
         return -1;
     }
 };
