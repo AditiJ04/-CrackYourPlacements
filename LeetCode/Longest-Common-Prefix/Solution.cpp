8860de2fca2ed74470d031ef0a9621605d536ab1
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-09-29 02:35:20 +0530

    Add solution for Longest Common Prefix

--- LeetCode/Longest-Common-Prefix/Solution.cpp
+++ LeetCode/Longest-Common-Prefix/Solution.cpp
@@ -1,34 +1,10 @@
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans="";
-        int n=strs.size();
-        for(int i=0;i<strs[0].length();i++)
-        {
-            char ch=strs[0][i];
-
-            bool match=true;
-
-            for(int j=1;j<n;j++)
-            {
-                if(strs[j].size()<i || ch!=strs[j][i])
-                {
-                    match=false;
-                    break;
-                }
-            }
-
-            if(match==false)
-            {
-                break;
-            }
-
-            else
-            {
-                ans.push_back(ch);
-            }
-        }
-
-        return ans;
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (String s : strs)
+            while (s.indexOf(prefix) != 0)
+                prefix = prefix.substring(0, prefix.length() - 1);
+        return prefix;
     }
-};
+}
