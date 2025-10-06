5f91c46c0df2aeecb130b05d7350fa77668e7353
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-10-07 01:34:18 +0530

    Add solution for Shortest Path in Binary Matrix

--- LeetCode/Shortest-Path-in-Binary-Matrix/Solution.cpp
+++ LeetCode/Shortest-Path-in-Binary-Matrix/Solution.cpp
@@ -1,45 +1,66 @@
 class Solution {
 public:
-    int n;
-
-    int row[8]={0,-1,0,1,-1,-1,1,1};
-    int col[8]={-1,0,1,0,-1,1,1,-1};
+    
+   vector<vector<int>>dirs={{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        n = grid.size();
-
-        if (grid[0][0] == 1 || grid[n-1][n-1]==1)
-            return -1;
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>> pq;
-
-        
-        dist[0][0]=1;
-
-        pq.push({1,{0,0}});
-
-        while(!pq.empty())
+        int n=grid.size();
+        int m=grid[0].size();
+        if(m==0 || n==0 || grid[0][0]==1)
         {
-            int dis=pq.top().first;
-            int r=pq.top().second.first;
-            int c=pq.top().second.second;
-            pq.pop();
+            return -1;
+        }
 
-            if(r==n-1 && c==n-1) return dis;
-            for(int k=0;k<8;k++)
+       // int row[]={0,-1,-1,-1,0,1,1,1};
+        //int col[]={-1,-1,0,1,1,1,0,-1};
+
+        auto issafe=[&](int x,int y)
+        {
+            if(x>=0 && x<n && y>=0 && y<m)
             {
-                int i=r+row[k];
-                int j=c+col[k];
-
-                if(i<0 || j<0 || i>=n || j>=n || grid[i][j]) continue;
-
-                if(dis+1<dist[i][j])
-                {
-                    dist[i][j]=dis+1;
-                    pq.push({dist[i][j],{i,j}});
-                }
+                return true;
             }
+
+            return false;
+        };
+        queue<pair<int,int>>que;
+        que.push({0,0});
+
+        grid[0][0]=1;
+
+        int lev=0;
+
+        while(!que.empty())
+        {
+            int N=que.size();
+
+            while(N--)
+            {
+                auto curr=que.front();
+                que.pop();
+
+                int x=curr.first;
+                int y=curr.second;
+
+                if(x==n-1 && y==m-1)
+                {
+                    return lev+1;
+                }
+
+                for(auto dir:dirs)
+                {
+                    int x_=x+dir[0];
+                    int y_=y+dir[1];
+
+                    if(issafe(x_,y_) && grid[x_][y_]==0)
+                    {
+                        que.push({x_,y_});
+                        grid[x_][y_]=1;
+                    }
+                }
+
+            }
+
+            lev++;
         }
 
         return -1;
