06095f4ae0aba2ba12c299d50c7b6f171d9753eb
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-10-09 15:35:28 +0530

    Add solution for Number of Operations to Make Network Connected

--- LeetCode/Number-of-Operations-to-Make-Network-Connected/Solution.cpp
+++ LeetCode/Number-of-Operations-to-Make-Network-Connected/Solution.cpp
@@ -1,40 +1,85 @@
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+	DisjointSet(int n) {
+		rank.resize(n + 1, 0);
+		size.resize(n + 1);
+		parent.resize(n + 1);
+
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+
+	// finding ultimate parent
+	int findUltimateParent(int node) {
+		if (node == parent[node])
+			return node;
+		return parent[node] = findUltimateParent(parent[node]);
+	}
+
+	void unionByRank(int u, int v) {
+		int UltimateParent_u = findUltimateParent(u);
+		int UltimateParent_v = findUltimateParent(v);
+
+		if (UltimateParent_u == UltimateParent_v)
+			return;
+
+		if (rank[UltimateParent_u] < rank[UltimateParent_v])
+			parent[UltimateParent_u] = UltimateParent_v;
+		else if (rank[UltimateParent_v] < rank[UltimateParent_u])
+			parent[UltimateParent_v] = UltimateParent_u;
+		else {
+			parent[UltimateParent_v] = UltimateParent_u;
+			rank[UltimateParent_u]++;
+		}
+	}
+
+	void unionBySize(int u, int v) {
+		int UltimateParent_u = findUltimateParent(u);
+		int UltimateParent_v = findUltimateParent(v);
+
+		if (UltimateParent_u == UltimateParent_v)
+			return;
+
+		if (rank[UltimateParent_u] < rank[UltimateParent_v]) {
+			parent[UltimateParent_u] = UltimateParent_v;
+			size[UltimateParent_v] += size[UltimateParent_u];
+		} else {
+			parent[UltimateParent_v] = UltimateParent_u;
+			size[UltimateParent_u] += size[UltimateParent_v];
+		}
+	}
+};
+
 class Solution {
 public:
-
-    void dfs(vector<vector<int>>&adj,vector<int>&vis,int node)
-    {
-        if(vis[node]) return;
-        vis[node]=1;
-        for(auto it:adj[node])
-        {
-            dfs(adj,vis,it);
-        }
-    }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        vector<vector<int>>adj(n);
-
-        int m=connections.size();
-
-        if(n-1>m) return -1;
-
-        for(int i=0;i<m;i++)
-        {
-            adj[connections[i][0]].push_back(connections[i][1]);
-            adj[connections[i][1]].push_back(connections[i][0]);
-        }
-
-        vector<int>vis(n,0);
-        int cnt=0;
-
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                cnt++;
-                dfs(adj,vis,i);
+    int makeConnected(int n, vector<vector<int>>& edge) {
+        DisjointSet ds(n);
+        
+        int cntExtras = 0;
+        for(auto it: edge) {
+            int u = it[0];
+            int v = it[1];
+            
+            if(ds.findUltimateParent(u) == ds.findUltimateParent(v)) {
+                cntExtras++;
+            } else { 
+                ds.unionBySize(u, v);
             }
         }
-
-        return cnt-1;
+        
+        int cnt = 0;
+        for(int i=0;i<n;i++) {
+            if(ds.parent[i] == i) {
+                cnt++;
+            }
+        }
+        int ans = cnt -1; 
+        if(cntExtras >= ans) {
+            return ans;
+        }
+        return -1;
     }
 };
