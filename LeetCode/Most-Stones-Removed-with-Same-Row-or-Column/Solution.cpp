5b86d1e206c8b61e9b1e0f679b39c84d6598e533
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-10-10 13:51:04 +0530

    Add solution for Most Stones Removed with Same Row or Column

--- LeetCode/Most-Stones-Removed-with-Same-Row-or-Column/Solution.cpp
+++ LeetCode/Most-Stones-Removed-with-Same-Row-or-Column/Solution.cpp
@@ -1,36 +1,73 @@
-class Solution {
-public:
-    
-    void dfs(vector<vector<int>>&stones,int idx,vector<bool>&vis)
-    {
-        vis[idx]=true;
-        for(int k=0;k<stones.size();k++)
-        {
-            int r=stones[idx][0];
-            int c=stones[idx][1];
-            if((vis[k]==false) && (stones[k][0]==r || stones[k][1]==c))
-            {
-                dfs(stones,k,vis);
-            }
-        }
-    }
-    int removeStones(vector<vector<int>>& stones) {
-        int n=stones.size();
-        int grps=0;
+class Disjoint{
+    vector<int>parent,size;
 
-        vector<bool>vis(n,false);
+    public:
+
+    Disjoint(int n)
+    {
+        parent.resize(n);
+        size.resize(n);
 
         for(int i=0;i<n;i++)
         {
-            if(vis[i]==true)
-            {
-                continue;
-            }
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
 
-            dfs(stones,i,vis);
-            grps++;
+    int findUParent(int node)
+    {
+        if(node==parent[node])
+        {
+            return node;
+        }
+        return parent[node]=findUParent(parent[node]);
+    }
+
+    void unionBySize(int u,int v)
+    {
+        int ulp_u=findUParent(u);
+        int ulp_v=findUParent(v);
+
+        if(size[ulp_u]<size[ulp_v])
+        {
+            size[ulp_v]+=size[ulp_u];
+            parent[ulp_u]=ulp_v;
+        }
+        else
+        {
+            size[ulp_u]+=size[ulp_v];
+            parent[ulp_v]=ulp_u;
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+
+        Disjoint ds(n);
+        int cnt=0;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1])
+                {
+                    int u=ds.findUParent(i);
+                    int v=ds.findUParent(j);
+
+                    if(u!=v)
+                    {
+                        ds.unionBySize(u,v);
+                        cnt++;
+                    }
+                }
+            }
         }
 
-        return n-grps;
+        return cnt;
     }
 };
