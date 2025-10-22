e5ce245b031ccdfed1219c353e39b01040e84959
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-10-22 14:17:57 +0530

    Add solution for Valid Perfect Square

--- LeetCode/Valid-Perfect-Square/Solution.cpp
+++ LeetCode/Valid-Perfect-Square/Solution.cpp
@@ -3,11 +3,27 @@ public:
     bool isPerfectSquare(int num) {
 
         if(num==1) return true;
-        for(long long i=1;i<=num/2;i++)
+        int l=1;
+        int h=num-1;
+
+        while(l<=h)
         {
-            if(i*i==(long long)num) return true;
+            long long mid=(l+h)/2;
+
+            if(mid*mid==(long long)num)return true;
+
+            else if(mid*mid>(long long)num)
+            {
+                h=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
         }
 
         return false;
     }
 };
+
+// 1 13
