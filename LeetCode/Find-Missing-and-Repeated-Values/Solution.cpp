9d90bee8c97f3f396fecb614fdddacf031bb06f3
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-10-28 10:20:01 +0530

    Add solution for Find Missing and Repeated Values

---
+++ LeetCode/Find-Missing-and-Repeated-Values/Solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int a, b;
+        vector<int> ans;
+        vector<int> v(n * n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                v[grid[i][j]]++;
+            }
+        }
+
+        for (int i = 1; i <= n * n; i++) {
+            if (v[i] == 2) {
+                a = i;
+            }
+            if (v[i] == 0) {
+                b = i;
+            }
+        }
+
+        ans.push_back(a);
+        ans.push_back(b);
+
+        return ans;
+    }
+};
