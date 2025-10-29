a2d415edfb27686018bd3e0309dde868b8e8af57
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-10-29 12:34:26 +0530

    Add solution for Majority Element II

--- LeetCode/Majority-Element-II/Solution.cpp
+++ LeetCode/Majority-Element-II/Solution.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>element;
+        int n = nums.size();
 
-        map<int,int>mp;
+        vector<int> v;
 
-        int n=nums.size();
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = 0, el2 = 0;
 
-        int k=floor(n/3);
-
-        for(int i=0;i<n;i++)
-        {
-            mp[nums[i]]++;
-        }
-        
-        for(auto c:mp)
-        {
-            if(c.second>k)
-            {
-                element.push_back(c.first);
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && nums[i] != el2) {
+                cnt1 = 1;
+                el1 = nums[i];
             }
+        else if (cnt2 == 0 && el1 != nums[i]) {
+            cnt2 = 1;
+            el2 = nums[i];
         }
-        return element;
+        else if (nums[i] == el1) {
+            cnt1++;
+        }
+        else if (nums[i] == el2) {
+            cnt2++;
+        }
+
+        else {
+            cnt1--;
+            cnt2--;
+        }
     }
-};
+
+    int c1 = 0, c2 = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == el1)
+            c1++;
+        else if (nums[i] == el2)
+            c2++;
+    }
+
+    if (c1 > (n / 3))
+        v.push_back(el1);
+    if (c2 > (n / 3))
+        v.push_back(el2);
+
+    return v;
+}
+}
+;
