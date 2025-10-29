65454d753cf8eec43b689b4635eafc8607a935c2
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-10-29 14:37:37 +0530

    Add solution for Unique Paths

--- LeetCode/Unique-Paths/Solution.cpp
+++ LeetCode/Unique-Paths/Solution.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int dp[101][101];
-    int CountWays(int i,int j)
-    {
-        if(i==0 || j==0)
-        {
-            return 1;
-        }
-
-        if(i<0 || j<0)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-
-        int left=CountWays(i,j-1);
-        int up=CountWays(i-1,j);
-
-        return dp[i][j]=left+up;
-    }
     int uniquePaths(int m, int n) {
-        
-        memset(dp,-1,sizeof(dp));
+    
+        vector<vector<int>>dp(m,vector<int>(n,0));
 
-       return CountWays(m-1,n-1);
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+
+                if(i==m-1 || j==n-1)
+                {
+                    dp[i][j]=1;
+                }
+                else
+                {
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
