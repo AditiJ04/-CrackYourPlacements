40b610d5d40d8a1deb187b0350a60cfa3c898079
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-11-05 22:45:50 +0530

    Add solution for Third Maximum Number

--- LeetCode/Third-Maximum-Number/Solution.cpp
+++ LeetCode/Third-Maximum-Number/Solution.cpp
@@ -2,30 +2,24 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         int n=nums.size();
-
-        long long mx1=LLONG_MIN;
-        long long mx2=LLONG_MIN;
-        long long mx3=LLONG_MIN;
+        set<int>s;
+      //  sort(nums.begin(),nums.end());
 
         for(int i=0;i<n;i++)
         {
-            if(nums[i]>mx1)
-            {
-                mx3=mx2;
-                mx2=mx1;
-                mx1=nums[i];
-            }
-            else if(nums[i]>mx2 && nums[i]!=mx1)
-            {
-                mx3=mx2;
-                mx2=nums[i];
-            }
-            else if(nums[i]>mx3 && nums[i]!=mx2 && nums[i]!=mx1)
-            {
-                mx3=nums[i];
-            }
+            s.insert(nums[i]);
+        }
+        vector<int>v(s.begin(),s.end());
+        sort(v.begin(),v.end(),greater<int>());
+
+        if(v.size()>=3)
+        {
+            return v[2];
+        }
+        else
+        {
+            return v[0];
         }
 
-        return mx3==LLONG_MIN?mx1:mx3;
     }
 };
