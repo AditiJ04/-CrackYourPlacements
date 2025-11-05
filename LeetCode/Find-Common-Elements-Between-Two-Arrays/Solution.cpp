a726933fd716d349c62df97ad1f14766cd607566
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-11-05 22:58:11 +0530

    Add solution for Find Common Elements Between Two Arrays

--- LeetCode/Find-Common-Elements-Between-Two-Arrays/Solution.cpp
+++ LeetCode/Find-Common-Elements-Between-Two-Arrays/Solution.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp1;
-        unordered_map<int,int>mp2;
+        map<int,int>mp1;
+        map<int,int>mp2;
 
-        int n=nums1.size();
-        int m=nums2.size();
+        int cnt1=0;
+        int cnt2=0;
 
-        for(int i=0;i<n;i++)
+        vector<int>v(2,0);
+
+        for(int i=0;i<nums1.size();i++)
         {
             mp1[nums1[i]]++;
         }
 
-        for(int i=0;i<m;i++)
+        for(int i=0;i<nums2.size();i++)
         {
             mp2[nums2[i]]++;
         }
 
-        int c1=0,c2=0;
-
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums2.size();i++)
         {
-            if(mp2.find(nums1[i])!=mp2.end()) c1++;
-        }
-        for(int i=0;i<m;i++)
-        {
-            if(mp1.find(nums2[i])!=mp1.end()) c2++;
+            if(mp1[nums2[i]]>=1)
+            {
+                v[1]++;
+            }
         }
 
-        return {c1,c2};
+        for(int i=0;i<nums1.size();i++)
+        {
+            if(mp2[nums1[i]]>=1)
+            {
+                v[0]++;
+            }
+        }
+       
+        return v;
     }
 };
-
-// 2 2 3
-// 1 2
