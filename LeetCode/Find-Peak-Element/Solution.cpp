ea7dc553ba8021e79f0aa2547f4bad7010f1d9e4
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-11-11 17:25:46 +0530

    Add solution for Find Peak Element

--- LeetCode/Find-Peak-Element/Solution.cpp
+++ LeetCode/Find-Peak-Element/Solution.cpp
@@ -4,24 +4,29 @@ public:
         int n=nums.size();
 
         if(n==1) return 0;
-        if(nums[1]<nums[0]) return 0;
-        if(nums[n-2]<nums[n-1]) return n-1;
-
-        int l=1,h=n-2;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        int l=0;
+        int h=n-1;
+        int mid;
 
         while(l<=h)
         {
-            int mid=(l+h)/2;
+            mid=(l+h)/2;
 
-            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
+            if((mid-1>=0) && (mid+1<n) &&
+            nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
 
-            if(nums[mid-1]<nums[mid]) l=mid+1;
+            if(nums[mid]<nums[mid+1])
+            {
+                l=mid+1;
+            }
             else
             {
                 h=mid-1;
             }
         }
 
-        return -1;
+        return mid;
     }
 };
