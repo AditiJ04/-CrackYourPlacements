d9f5b0149befa1cd637c22ed740235503ec1b06b
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-11-13 16:59:55 +0530

    Add solution for Heaters

--- LeetCode/Heaters/Solution.cpp
+++ LeetCode/Heaters/Solution.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
+    
+    bool blackBox(vector<int>& houses, vector<int>& heaters, int n, int m, int radius){
+        int i=0, j=0; //2 pointers
+        
+        while(i<n){
+            if(j == m){ 
+                return false;//coz we have no heater left to cover rest of the houses, so this cant be answer
+            }
+            
+            int l = heaters[j] - radius; //get the left range
+            int r = heaters[j] + radius; //get the right range
+            if(houses[i] < l or houses[i] > r){
+                j++; //if current house is not in the range, check with next heater
+            }
+            else{
+                i++; //if current heater can cover the current house then check for the next house
+            }
+        }
+        
+        return true; //if i==n states that we are able to cover all the houses with a given number of heater(can be 1 or all) so this mid as radiuse can be one answer
+    }
+    
     int findRadius(vector<int>& houses, vector<int>& heaters) {
-
-        int n=houses.size();
-        int m=heaters.size();
-
         sort(houses.begin(),houses.end());
         sort(heaters.begin(),heaters.end());
-
-        int mx=INT_MIN;
-    
-        for(int i=0;i<n;i++)
-        {
-            int prev=0;
-            int mn=INT_MAX;
-
-            for(int j=0;j<m;j++)
-            {
-                int diff=abs(houses[i]-heaters[j]);
-               // if(diff==0) continue;
-                mn=min(diff,mn);
+        
+        int n = houses.size();
+        int m = heaters.size();
+        
+        int low = 0; //cann't define as 1, coz there will be a case where all the heaters will be at all the houses resp.
+        int high = max(*max_element(houses.begin(),houses.end()),*max_element(heaters.begin(),heaters.end())); //to reduce search space, will also work if defined as 1e9
+        
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            cout<<mid<<endl;
+            if(blackBox(houses,heaters,n,m,mid)==true){ //called the blackbox to find that is it possible for current radius to be the ans
+                ans = mid;
+                high = mid - 1; //to minimise the ans;
             }
-            mx=max(mx,mn);
+            else{
+                low = mid + 1; //go to the right coz this mid can't be one answer;
+            }
         }
-
-        return mx;
+        return ans;
     }
 };
