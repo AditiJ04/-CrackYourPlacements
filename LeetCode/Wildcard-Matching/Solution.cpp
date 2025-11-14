aa9634dbc578ec074210b06db6939e1f7eff0a60
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-11-14 08:37:40 +0530

    Add solution for Wildcard Matching

--- LeetCode/Wildcard-Matching/Solution.cpp
+++ LeetCode/Wildcard-Matching/Solution.cpp
@@ -1,103 +1,29 @@
 class Solution {
 public:
-
-    bool tab(string s,string p)
-    {
-
-        int n=s.size();
-        int m=p.size();
-
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-
-        dp[0][0]=true;
-
-        for(int j=1;j<=m;j++)
-        {
-            bool flg=true;
-            for(int k=1;k<=j;k++)
-            {
-                if(p[k-1]!='*')
-                {
-                    flg=false;
-                    break;
-                }
+    bool solve(int index1, int index2, string &text, string &pattern) {
+        if (index1 < 0 && index2 < 0) return true;
+        if (index2 < 0 && index1 >= 0) return false;
+        if (index1 < 0 && index2 >= 0) {
+            for (int i = 0; i <= index2; i++) {
+                if (pattern[i] != '*') return false;
             }
-            dp[0][j]=flg;
+            return true;
         }
 
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s[i-1]==p[j-1] || p[j-1]=='?')
-                {
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else if(p[j-1]=='*')
-                {
-                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
-                }
-                else
-                {
-                    dp[i][j]=false;
-                }
-            }
+        if (text[index1] == pattern[index2] || pattern[index2] == '?') {
+            return solve(index1 - 1, index2 - 1, text, pattern);
         }
 
-        return dp[n][m];
+        if (pattern[index2] == '*') {
+            return solve(index1 - 1, index2, text, pattern) || solve(index1, index2 - 1, text, pattern);
+        }
+
+        return false;
     }
-    /*bool solve(string s,string p,int i,int j, vector<vector<int>>&dp)
-    {
-        if(i==0 && j==0)
-        {
-            return true;
-        }
 
-        if(i>0 && j==0)
-        {
-            return false;
-        }
-
-        if(i==0 && j>0)
-        {
-            for(int k=1;k<=j;k++)
-            {
-                if(p[k-1]!='*')
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-
-        if(s[i-1]==p[j-1] || p[j-1]=='?')
-        {
-            return dp[i][j]=solve(s,p,i-1,j-1,dp);
-        }
-
-        else if(p[j-1]=='*')
-        {
-            return dp[i][j]=solve(s,p,i,j-1,dp) || solve(s,p,i-1,j,dp);
-        }
-
-        else
-        {
-            return false;
-        }
-    }*/
     bool isMatch(string s, string p) {
-
-        int n=s.size();
-        int m=p.size();
-
-      //  vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-       // return solve(s,p,s.size(),p.size(),dp);
-       return tab(s,p);
+        int n1 = s.size();
+        int n2 = p.size();
+        return solve(n1-1, n2-1, s, p);
     }
 };
