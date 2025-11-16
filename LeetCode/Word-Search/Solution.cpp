fc7024cac5adce95f7a42fdd61cb2cebbba2dd89
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-11-16 11:37:40 +0530

    Add solution for Word Search

--- LeetCode/Word-Search/Solution.cpp
+++ LeetCode/Word-Search/Solution.cpp
@@ -1,44 +1,65 @@
 class Solution {
 public:
-
-    int row[4]={0,-1,0,1};
-    int col[4]={-1,0,1,0};
-
-    int n,m;
-    bool findWord(vector<vector<char>>&board,vector<vector<int>>&vis,string word,int idx,int i,int j)
+    bool Search(int i,int j,vector<vector<char>>&board,vector<vector<int>>&vis,string word,int idx)
     {
-        if(i<0 || j<0 || i==n || j==m) return false;
-        if(idx==word.size()) return true;
+        int n=board.size();
+        int m=board[0].size();
+
+        if(idx==word.size())
+        {
+            return true;
+        }
+
+         if (i < 0 || j < 0 || i >= n || j >= m || vis[i][j] || board[i][j] != word[idx])
+        return false;
 
         vis[i][j]=1;
+
+        int row[4]={0,-1,0,1};
+        int col[4]={-1,0,1,0};
+
         for(int k=0;k<4;k++)
         {
             int nr=i+row[k];
             int nc=j+col[k];
 
-            if(nr<0 || nc<0 || nr==n || nc==m || vis[nr][nc]) continue;
-
-            if(board[nr][nc]==word[idx])
+            if(nr>=0 && nr<n && nc>=0 && nc<m)
             {
-                if(findWord(board,vis,word,idx+1,nr,nc)) return true;
+                if(Search(nr,nc,board,vis,word,idx+1))
+                {
+                    return true;
+                }
             }
         }
+
         vis[i][j]=0;
         return false;
+
     }
     bool exist(vector<vector<char>>& board, string word) {
-        n=board.size();
-        m=board[0].size();
+        int n=board.size();
+        int m=board[0].size();
 
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        if(n==1 && word.size()==1)
+        {
+            if(board[0][0]==word[0])
+            {
+                return true;
+            }
 
+            return false;
+        }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(board[i][j]==word[0])
                 {
-                    if(findWord(board,vis,word,1,i,j)) return true;
+                     vector<vector<int>>vis(n,vector<int>(m,0));
+                    if(Search(i,j,board,vis,word,0))
+                    {
+                        return true;
+                    }
                 }
             }
         }
