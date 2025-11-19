0f4c73acae2376a7c73fa03fd39ec5dc357a1b0a
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-11-19 10:33:38 +0530

    Add solution for Min Stack

--- LeetCode/Min-Stack/Solution.cpp
+++ LeetCode/Min-Stack/Solution.cpp
@@ -1,41 +1,14 @@
 class MinStack {
-    stack<pair<int,int>>st;
+    stack<int> s, st;
 public:
-    MinStack() {
-        
-    }
-    
     void push(int val) {
-        int Min;
-        if(st.empty())
-        {
-            Min=val;
-        }
-        else
-        {
-            Min=min(st.top().second,val);
-        }
-        st.push({val,Min});
+        s.push(val);
+        if (st.empty() || val <= st.top()) st.push(val);
     }
-    
     void pop() {
-        st.pop();
+        if (s.top() == st.top()) st.pop();
+        s.pop();
     }
-    
-    int top() {
-        return st.top().first;
-    }
-    
-    int getMin() {
-        return st.top().second;
-    }
+    int top() { return s.empty() ? -1 : s.top(); }
+    int getMin() { return st.empty() ? -1 : st.top(); }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
