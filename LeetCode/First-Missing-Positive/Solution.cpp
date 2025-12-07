3ad9d607dcf86973b560509962b056c0ef16720a
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-12-08 02:17:00 +0530

    Add solution for First Missing Positive

--- LeetCode/First-Missing-Positive/Solution.cpp
+++ LeetCode/First-Missing-Positive/Solution.cpp
@@ -1,26 +1,31 @@
 1class Solution {
 2public:
 3    int firstMissingPositive(vector<int>& nums) {
-4        int n=nums.size();
+4       int n=nums.size();
 5
-6        unordered_map<int,int>mp;
-7        for(int i=0;i<n;i++)
-8        {
-9            mp[nums[i]]++;
-10        }
-11        int num=1;
-12
-13        for(auto it:mp)
-14        {
-15            if(mp.find(num)!=mp.end()){
-16                num++;
-17            }
-18            else
-19            {
-20                break;
-21            }
-22        }
-23
-24        return num;
-25    }
-26};
+6       int mx=0;
+7
+8       unordered_map<int,int>mp;
+9       for(int i=0;i<n;i++)
+10       {
+11        mp[nums[i]]++;
+12       } 
+13
+14       for(int i=0;i<n;i++)
+15       {
+16        if(nums[i]>mx)
+17        {
+18            mx=nums[i];
+19        }
+20       }
+21
+22       for(int i=1;i<=mx;i++)
+23       {
+24        if(mp.find(i)==mp.end())
+25        {
+26            return i;
+27        }
+28       }
+29       return mx+1;
+30    }
+31};
