8eb721de441bc24f092b80bea6b56ef758f16b3b
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-12-07 10:41:08 +0530

    Add solution for Reverse Linked List

--- LeetCode/Reverse-Linked-List/Solution.cpp
+++ LeetCode/Reverse-Linked-List/Solution.cpp
@@ -1,28 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* p=head;
-        ListNode* q=NULL;
-        ListNode* r=NULL;
-
-        while(p!=NULL)
-        {
-            q=p;
-            p=p->next;
-            q->next=r;
-            r=q;
-        }
-
-        return q;
-    }
-};
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* reverseList(ListNode* head) {
+14        ListNode* p=head;
+15        ListNode* q=NULL;
+16        ListNode* r=NULL;
+17
+18        while(p!=NULL)
+19        {
+20            q=p;
+21            p=p->next;
+22            q->next=r;
+23            r=q;
+24        }
+25
+26        return q;
+27    }
+28};
