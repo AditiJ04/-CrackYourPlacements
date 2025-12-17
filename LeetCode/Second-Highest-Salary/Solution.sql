e7abc8ff8fe9c42c11851d06c69f1a089e680165
Aditi <151827388+AditiJ04@users.noreply.github.com>
2025-12-18 00:16:14 +0530

    Add solution for Second Highest Salary

--- LeetCode/Second-Highest-Salary/Solution.sql
+++ LeetCode/Second-Highest-Salary/Solution.sql
@@ -1,3 +1,4 @@
 1# Write your MySQL query statement below
 2select (select distinct salary from Employee
-3order by salary desc LIMIT 1 OFFSET 1) as SecondHighestSalary;
+3order by salary desc
+4LIMIT 1 offset 1) as SecondHighestSalary
