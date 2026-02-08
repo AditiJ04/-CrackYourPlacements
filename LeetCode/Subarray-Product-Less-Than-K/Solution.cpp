8eadfe4c66ea39a743f3c5d5c3fac57378d6afa9
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-02-08 15:54:33 +0530

    Add solution for Subarray Product Less Than K

--- LeetCode/Subarray-Product-Less-Than-K/Solution.cpp
+++ LeetCode/Subarray-Product-Less-Than-K/Solution.cpp
@@ -1,27 +1,28 @@
 1class Solution {
 2public:
 3    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-4        int cnt=0;
-5        for(int i=0;i<nums.size();i++)
-6        {
-7            int pro=nums[i];
-8            if(pro<k)
-9            {
-10                cnt++;
-11            }
-12            for(int j=i+1;j<nums.size();j++)
-13            {
-14                pro*=nums[j];
-15                if(pro<k)
-16                {
-17                    cnt++;
-18                }
-19                else
-20                {
-21                    break;
-22                }
-23            }
-24        }
-25        return cnt;
-26    }
-27};
+4        int n=nums.size();
+5
+6        int i=0;
+7
+8        int j=0;
+9        int pro=1,cnt=0;
+10        if(k<=1)
+11        {
+12            return 0;
+13        }
+14        while(j<n)
+15        {
+16            pro*=nums[j];
+17
+18            while(pro>=k)
+19            {
+20                pro/=nums[i];
+21                i++;
+22            }
+23            cnt+=j-i+1;
+24            j++;
+25        }
+26        return cnt;
+27    }
+28};
