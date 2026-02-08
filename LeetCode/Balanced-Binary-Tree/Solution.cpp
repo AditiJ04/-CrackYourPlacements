65b4661136cf9f111130486996b3db3e342c1925
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-02-08 11:45:10 +0530

    Add solution for Balanced Binary Tree

--- LeetCode/Balanced-Binary-Tree/Solution.cpp
+++ LeetCode/Balanced-Binary-Tree/Solution.cpp
@@ -1,36 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int height(TreeNode* root)
-    {
-        if(root==NULL) return 0;
-
-        int lh=height(root->left);
-        int rh=height(root->right);
-
-        return 1+max(lh,rh);
-    }
-    bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
-
-        bool l,h;
-        l=isBalanced(root->left);
-        h=isBalanced(root->right);
-
-        bool ans=abs(height(root->left)-height(root->right))<=1;
-
-        if(l && h && ans) return true;
-
-        return false;
-    }
-};
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    int height(TreeNode* root)
+15    {
+16        if(root==NULL) return 0;
+17
+18        int lh=height(root->left);
+19        int rh=height(root->right);
+20
+21        return 1+max(lh,rh);
+22    }
+23    bool isBalanced(TreeNode* root) {
+24        if(root==NULL) return true;
+25
+26        bool l,h;
+27        l=isBalanced(root->left);
+28        h=isBalanced(root->right);
+29
+30        bool ans=abs(height(root->left)-height(root->right))<=1;
+31
+32        if(l && h && ans) return true;
+33
+34        return false;
+35    }
+36};
