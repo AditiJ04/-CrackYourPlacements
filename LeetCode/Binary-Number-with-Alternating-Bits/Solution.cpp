c3ce1e1c6a6162b7b16486780b94b214c082845c
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-02-18 21:19:40 +0530

    Add solution for Binary Number with Alternating Bits

--- LeetCode/Binary-Number-with-Alternating-Bits/Solution.cpp
+++ LeetCode/Binary-Number-with-Alternating-Bits/Solution.cpp
@@ -1,15 +1,7 @@
 1class Solution {
 2public:
 3    bool hasAlternatingBits(int n) {
-4        int prev=-1;
-5        while(n!=0)
-6        {
-7            int r=n%2;
-8            if(prev!=-1 && r==prev) return false;
-9            prev=r;
-10            n/=2;
-11        }
-12
-13        return true;
-14    }
-15};
+4        long long x=n ^(n>>1);
+5        return (x & (x+1))==0;
+6    }
+7};
