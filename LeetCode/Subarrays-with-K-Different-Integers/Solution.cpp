aed04d29874fa0d89a877e25bdc8bc9172c453ee
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-03-03 20:03:13 +0530

    Add solution for Subarrays with K Different Integers

--- LeetCode/Subarrays-with-K-Different-Integers/Solution.cpp
+++ LeetCode/Subarrays-with-K-Different-Integers/Solution.cpp
@@ -1,35 +1,30 @@
 1class Solution {
 2public:
-3    
-4    int SubArray(vector<int>&nums,int k)
-5    {
-6        int n=nums.size();
-7        unordered_map<int,int>mp;
-8
-9        int i=0,j=0;
-10        int ans=0;
-11
-12        while(i<n)
-13        {
-14            mp[nums[i]]++;
-15            while(mp.size()>k)
-16            {
-17                mp[nums[j]]--;
+3    int findSubarrays(vector<int>& nums, int k) {
+4        int n = nums.size();
+5
+6        int i = 0, j = 0;
+7        unordered_map<int, int> mp;
+8        int ans = 0;
+9
+10        while (i < n) {
+11            mp[nums[i]]++;
+12            while (mp.size() > k) {
+13                mp[nums[j]]--;
+14                if (mp[nums[j]] == 0)
+15                    mp.erase(nums[j]);
+16                j++;
+17            }
 18
-19                if(mp[nums[j]]==0) mp.erase(nums[j]);
-20                j++;
+19            if (mp.size() <= k) {
+20                ans += i - j+1;
 21            }
-22
-23            if(mp.size()<=k)
-24            {
-25                ans+=i-j+1;
-26            }
-27            i++;
-28        }
-29
-30        return ans;
-31    }
-32    int subarraysWithKDistinct(vector<int>& nums, int k) {
-33        return SubArray(nums,k)-SubArray(nums,k-1);
-34    }
-35};
+22            i++;
+23        }
+24
+25        return ans;
+26    }
+27    int subarraysWithKDistinct(vector<int>& nums, int k) {
+28        return findSubarrays(nums, k)-findSubarrays(nums,k-1);
+29    }
+30};
