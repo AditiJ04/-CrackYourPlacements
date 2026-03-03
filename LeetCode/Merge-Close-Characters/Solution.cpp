f9b4897420ebeeb51cc51c055300c3591e99837e
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-03-04 00:00:00 +0530

    Add solution for Merge Close Characters

--- LeetCode/Merge-Close-Characters/Solution.cpp
+++ LeetCode/Merge-Close-Characters/Solution.cpp
@@ -1,28 +1,28 @@
 1class Solution {
 2public:
 3    string mergeCharacters(string s, int k) {
-4
-5        string str = s;
+4        
+5        string str=s;
 6
-7        int m = str.size();
-8        for (int i = 0; i < m;) {
-9            char c = str[i];
-10            int flg = 1;
-11            for (int j = i + 1; j < m; j++) {
-12                if (j - i > k)
-13                    break;
-14                char nc = str[j];
+7        int m=str.size();
+8        for(int i=0;i<m;)
+9        {
+10            char c=str[i];
+11            int flg=1;
+12            for(int j=i+1;j<=i+k && j<m;j++)
+13            {
+14                char nc=str[j];
 15
-16                if (nc == c) {
-17                    str.erase(j, 1);
-18                    flg = 0;
-19                    break;
-20                }
-21            }
-22            if (flg)
-23                i++;
+16                if(nc==c)
+17                {
+18                    str.erase(j,1);
+19                    flg=0;
+20                    break;
+21                }
+22            }
+23            if(flg) i++;
 24            else i=0;
-25            m = str.size();
+25            m=str.size();
 26        }
 27
 28        return str;
