837ace64b72abb3b11127c225a778f8ab83b9c7f
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-03-09 22:46:21 +0530

    Add solution for Find Peak Element

--- LeetCode/Find-Peak-Element/Solution.cpp
+++ LeetCode/Find-Peak-Element/Solution.cpp
@@ -1,32 +1,34 @@
-class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-
-        if(n==1) return 0;
-        if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
-        int l=0;
-        int h=n-1;
-        int mid;
-
-        while(l<=h)
-        {
-            mid=(l+h)/2;
-
-            if((mid-1>=0) && (mid+1<n) &&
-            nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) return mid;
-
-            if(nums[mid]<nums[mid+1])
-            {
-                l=mid+1;
-            }
-            else
-            {
-                h=mid-1;
-            }
-        }
-
-        return mid;
-    }
-};
+1class Solution {
+2public:
+3    int findPeakElement(vector<int>& nums) {
+4        int n=nums.size();
+5
+6        if(n==1) return 0;
+7
+8        if(nums[0]>nums[1]) return 0;
+9        if(nums[n-2]<nums[n-1]) return n-1;
+10
+11        int l=1,h=n-2;
+12
+13        while(l<=h)
+14        {
+15            int mid=(l+h)/2;
+16
+17            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+18            {
+19                return mid;
+20            }
+21            else if(nums[mid]<nums[mid+1]) l=mid+1;
+22            else
+23            {
+24                h=mid-1;
+25            }
+26        }
+27
+28        return l;
+29    }
+30};
+31
+32// 1 2 3 1
+33// 0 1 2 3
+34
