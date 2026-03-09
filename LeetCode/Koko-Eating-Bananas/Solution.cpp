2c481ce4bb1de5bfaa53e382964d5bb5a64c436a
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-03-09 23:50:09 +0530

    Add solution for Koko Eating Bananas

--- LeetCode/Koko-Eating-Bananas/Solution.cpp
+++ LeetCode/Koko-Eating-Bananas/Solution.cpp
@@ -1,38 +1,39 @@
 1class Solution {
 2public:
-3     long long findEatingSpeed(vector<int>&piles,int capacity)
-4    {
-5        long long cnt=0;
-6
-7        for(int i=0;i<piles.size();i++)
-8        {
-9            cnt+=(piles[i]%capacity==0)?(piles[i]/capacity):(piles[i]/capacity)+1;
-10        }
-11        return cnt;
-12    }
-13    int minEatingSpeed(vector<int>& piles, int h) {
-14        int n=piles.size();
-15
-16        int l=1;
-17        int r=*max_element(piles.begin(),piles.end());
-18
-19        int ans=0;
-20        while(l<=r)
-21        {
-22            int mid=(l+r)/2;
-23            long long cnt=findEatingSpeed(piles,mid);
-24
-25            if(cnt<=h)
-26            {
-27                ans=mid;
-28                r=mid-1;
-29            }
-30            else
-31            {
-32                l=mid+1;
-33            }
-34        }
-35
-36        return ans;
-37    }
-38};
+3    long long findHours(vector<int>& piles, int cap) {
+4        long long cnt = 0;
+5
+6        for (int i = 0; i < piles.size(); i++) {
+7            cnt += piles[i] / cap;
+8
+9            if (piles[i] % cap != 0) {
+10                cnt++;
+11            }
+12        }
+13
+14        return cnt;
+15    }
+16    int minEatingSpeed(vector<int>& piles, int h) {
+17        int n = piles.size();
+18        int mx = *max_element(piles.begin(), piles.end());
+19
+20        int s = 1, e = mx;
+21        int ans;
+22        
+23        while (s <= e) {
+24            int mid=(s+e)/2;
+25            long long hr = findHours(piles, mid);
+26            if(hr<=h)
+27            {
+28                ans=mid;
+29                e=mid-1;
+30            }
+31            else if(hr>h)
+32            {
+33                s=mid+1;
+34            }
+35        }
+36
+37        return ans;
+38    }
+39};
