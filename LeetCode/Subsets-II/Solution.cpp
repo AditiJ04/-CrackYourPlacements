eba412692f34205a11aa35ab2a3e4c69bd64e572
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-07-18 17:54:13 +0530

    Add solution for Subsets II

---
+++ LeetCode/Subsets-II/Solution.cpp
@@ -0,0 +1,30 @@
+1class Solution {
+2public:
+3    vector<vector<int>> ans;
+4    map<vector<int>, int> mp;
+5    void findSubsets(vector<int>& nums, int idx, vector<int> v) {
+6
+7        if (mp.find(v) == mp.end()) {
+8            ans.push_back(v);
+9        }
+10
+11        mp[v]++;
+12        if (idx == nums.size()) {
+13            return;
+14        }
+15        findSubsets(nums, idx + 1, v);
+16        v.push_back(nums[idx]);
+17        findSubsets(nums, idx + 1, v);
+18    }
+19    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+20        sort(nums.begin(), nums.end());
+21
+22        vector<int> v;
+23
+24        for (int i = 0; i < nums.size(); i++) {
+25            findSubsets(nums, i, v);
+26        }
+27
+28        return ans;
+29    }
+30};
