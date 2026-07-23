480505beca049693112d195eed5a87ac65dd1256
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-07-23 06:35:18 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.cpp
+++ LeetCode/Climbing-Stairs/Solution.cpp
@@ -1,21 +1,17 @@
 1class Solution {
 2public:
 3    int climbStairs(int n) {
-4
-5        if (n == 0 || n == 1 || n == 2)
-6            return n;
-7        vector<int> dp(n + 1, 0);
-8        dp[0] = 0;
-9        dp[1] = 1;
-10        dp[2]=2;
-11
-12        for (int i = 3; i <= n; i++) {
-13            dp[i] = dp[i - 2] + dp[i - 1];
-14        }
-15
-16        return dp[n];
-17    }
-18};
-19
-20//  0   1   2  3
-21//  0   1   1   2
+4        if(n==0 || n==1 || n==2) return n;
+5
+6        int a=1,b=2;
+7
+8        for(int i=3;i<=n;i++)
+9        {
+10            int c=a+b;
+11            a=b;
+12            b=c;
+13        }
+14
+15        return b;
+16    }
+17};
