68824fa54d31af678d78fdf024fb37c8cd173aa9
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-07-27 06:57:53 +0530

    Add solution for Minimum Path Sum

--- LeetCode/Minimum-Path-Sum/Solution.cpp
+++ LeetCode/Minimum-Path-Sum/Solution.cpp
@@ -1,26 +1,30 @@
 1class Solution {
 2public:
-3
-4    int findPath(int i,int j,vector<vector<int>>&dp,vector<vector<int>>&grid)
-5    {
-6        if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
+3    int minPathSum(vector<vector<int>>& grid) {
+4        if (grid.empty() || grid[0].empty()) return 0;
+5        int n = grid.size();
+6        int m = grid[0].size();
 7
-8        if(dp[i][j]!=-1) return dp[i][j];
-9        int res=INT_MAX;
-10        if((i+1)<grid.size()) {res=min(res,findPath(i+1,j,dp,grid));}
-11        if((j+1)<grid[0].size()) {res=min(res,findPath(i,j+1,dp,grid));}
-12
-13        return dp[i][j]=grid[i][j]+res;
-14    }
-15    int minPathSum(vector<vector<int>>& grid) {
-16
-17        if(grid.empty() || grid[0].empty()) return 0;
-18        int n=grid.size();
-19        int m=grid[0].size();
-20
-21        vector<vector<int>>dp(n,vector<int>(m,-1));
-22
-23        return findPath(0,0,dp,grid);
-24
-25    }
-26};
+8        vector<vector<int>> dp(n, vector<int>(m, 0));
+9        dp[n-1][m-1] = grid[n-1][m-1];
+10
+11        // last row
+12        for (int j = m - 2; j >= 0; j--) {
+13            dp[n-1][j] = dp[n-1][j+1] + grid[n-1][j];
+14        }
+15        // last column
+16        for (int i = n - 2; i >= 0; i--) {
+17            dp[i][m-1] = dp[i+1][m-1] + grid[i][m-1];
+18        }
+19
+20        // fill rest
+21        for (int i = n - 2; i >= 0; i--) {
+22            for (int j = m - 2; j >= 0; j--) {
+23                dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
+24            }
+25        }
+26
+27        return dp[0][0];
+28    }
+29};
+30
