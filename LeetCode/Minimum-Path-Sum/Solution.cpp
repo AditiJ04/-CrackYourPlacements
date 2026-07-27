73274ea2a1c5c934e65c7ee30d14474058afdb8b
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-07-27 07:03:04 +0530

    Add solution for Minimum Path Sum

--- LeetCode/Minimum-Path-Sum/Solution.cpp
+++ LeetCode/Minimum-Path-Sum/Solution.cpp
@@ -1,30 +1,34 @@
 1class Solution {
 2public:
 3    int minPathSum(vector<vector<int>>& grid) {
-4        if (grid.empty() || grid[0].empty()) return 0;
-5        int n = grid.size();
-6        int m = grid[0].size();
-7
-8        vector<vector<int>> dp(n, vector<int>(m, 0));
-9        dp[n-1][m-1] = grid[n-1][m-1];
+4        int n=grid.size();
+5        int m=grid[0].size();
+6
+7        vector<vector<int>>dp(n,vector<int>(m,0));
+8        dp[n-1][m-1]=grid[n-1][m-1];
+9
 10
-11        // last row
-12        for (int j = m - 2; j >= 0; j--) {
-13            dp[n-1][j] = dp[n-1][j+1] + grid[n-1][j];
+11        for(int i=n-2;i>=0;i--)
+12        {
+13            dp[i][m-1]=grid[i][m-1]+dp[i+1][m-1];
 14        }
-15        // last column
-16        for (int i = n - 2; i >= 0; i--) {
-17            dp[i][m-1] = dp[i+1][m-1] + grid[i][m-1];
-18        }
-19
-20        // fill rest
-21        for (int i = n - 2; i >= 0; i--) {
-22            for (int j = m - 2; j >= 0; j--) {
-23                dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
-24            }
-25        }
-26
-27        return dp[0][0];
-28    }
-29};
-30
+15
+16        for(int i=m-2;i>=0;i--)
+17        {
+18            dp[n-1][i]=grid[n-1][i]+dp[n-1][i+1];
+19        }
+20
+21        for(int i=n-2;i>=0;i--)
+22        {
+23            for(int j=m-2;j>=0;j--)
+24            {
+25                int c1=dp[i+1][j];
+26                int c2=dp[i][j+1];
+27
+28                dp[i][j]=min({c1,c2})+grid[i][j];
+29            }
+30        }
+31
+32        return dp[0][0];
+33    }
+34};
