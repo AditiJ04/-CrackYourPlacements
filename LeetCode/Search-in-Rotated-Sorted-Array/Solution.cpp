799978b2bbbb26d4294fd18e7450fb10ed33367a
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-09-06 12:10:10 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
@@ -9,30 +9,29 @@
 9        {
 10            int mid=(l+h)/2;
 11
-12            if(nums[mid]==target) return mid;
-13            if(nums[mid]>=nums[l])
+12            if(target==nums[mid]) return mid;
+13            if(nums[l]<=nums[mid])
 14            {
-15                if(target>=nums[l] && target<nums[mid])
-16                {
-17                    h=mid-1;
-18                }
-19                else
-20                {
-21                    l=mid+1;
-22                }
-23            }
-24            else{
-25                if(target>nums[mid] && target<=nums[h])
-26                {
-27                    l=mid+1;
-28                }
-29                else
-30                {
-31                    h=mid-1;
-32                }
-33            }
-34        }
+15                if(nums[l]<=target && target<nums[mid]) h=mid-1;
+16                else l=mid+1;
+17            }
+18            else
+19            {
+20                if(nums[mid]<target && nums[h]>=target)
+21                {
+22                    l=mid+1;
+23                }
+24                else
+25                {
+26                    h=mid-1;
+27                }
+28            }
+29        }
+30
+31        return -1;
+32    }
+33};
+34
 35
-36        return -1;
-37    }
-38};
+36//6 7 0 1 2 4 5
+37//0 1 2 3 4 5 6
