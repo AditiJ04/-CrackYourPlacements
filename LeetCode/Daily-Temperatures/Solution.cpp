a647ef9af38f5aeabff75e5df14b86ba305e73db
Aditi <151827388+AditiJ04@users.noreply.github.com>
2026-09-26 19:02:43 +0530

    Add solution for Daily Temperatures

--- LeetCode/Daily-Temperatures/Solution.cpp
+++ LeetCode/Daily-Temperatures/Solution.cpp
@@ -1,20 +1,30 @@
-class Solution {
-public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int>st;
-
-        int n=temperatures.size();
-        vector<int>v(n);
-
-        for(int i=0;i<n;i++)
-        {
-            while(st.size() && temperatures[st.top()]<temperatures[i])
-                {
-                    v[st.top()]=i-st.top();
-                    st.pop();
-                }
-               st.push(i);
-        }
-        return v;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> dailyTemperatures(vector<int>& temperatures) {
+4        int n=temperatures.size();
+5
+6        vector<int>ans(n,0);
+7        stack<int>st;
+8
+9        st.push(0);
+10
+11        for(int i=1;i<n;i++)
+12        {
+13            if(temperatures[i]<temperatures[st.top()])
+14            {
+15                st.push(i);
+16            }
+17            else
+18            {
+19                while(!st.empty() && temperatures[i]>temperatures[st.top()])
+20                {
+21                    ans[st.top()]=i-st.top();
+22                    st.pop();
+23                }
+24                st.push(i);
+25            }
+26        }
+27
+28        return ans;
+29    }
+30};
